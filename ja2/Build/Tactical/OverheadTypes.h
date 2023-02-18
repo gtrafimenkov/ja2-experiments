0196ab078c3f3cc2a79d61972b19c6b0949edad9
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2023-02-18 15:50:07 +0000

    Replace precompiled headers with explicit dependencies in every c file
    It slows down compilation, but makes dependencies between files clearer.

--- ja2/Build/Tactical/OverheadTypes.h
+++ ja2/Build/Tactical/OverheadTypes.h
@@ -2,6 +2,8 @@
 #define __OVERHEAD_TYPES_H
 // GLOBAL HEADER FOR DATA, TYPES FOR TACTICAL ENGINE
 
+#include "SGP/Types.h"
+
 #define REFINE_AIM_1 0
 #define REFINE_AIM_MID1 1
 #define REFINE_AIM_2 2
