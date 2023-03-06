1a2fea99c77c9f4226b4336da7fdf11123255caf
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2023-03-06 07:02:44 +0000

    Fix bug GetTeamSoldiers_Active

--- ja2lib/Team.c
+++ ja2lib/Team.c
@@ -7,21 +7,19 @@ void SetTeamSide(u8 teamID, u8 side) { gTacticalStatus.Team[teamID].bSide = side
 
 void GetTeamSoldiers(TeamID teamID, struct SoldierList* list) {
   const TacticalTeamType* team = &gTacticalStatus.Team[teamID];
-  list->num = team->bLastID - team->bFirstID + 1;
-  int counter = 0;
+  list->num = 0;
   for (int i = team->bFirstID; i <= team->bLastID; i++) {
-    list->soldiers[counter++] = MercPtrs[i];
+    list->soldiers[list->num++] = MercPtrs[i];
   }
 }
 
 void GetTeamSoldiers_Active(TeamID teamID, struct SoldierList* list) {
   const TacticalTeamType* team = &gTacticalStatus.Team[teamID];
-  list->num = team->bLastID - team->bFirstID + 1;
-  int counter = 0;
+  list->num = 0;
   for (int i = team->bFirstID; i <= team->bLastID; i++) {
     struct SOLDIERTYPE* sol = MercPtrs[i];
     if (IsSolActive(sol)) {
-      list->soldiers[counter++] = sol;
+      list->soldiers[list->num++] = sol;
     }
   }
 }
