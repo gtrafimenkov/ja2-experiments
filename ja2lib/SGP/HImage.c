gtrafimenkov/ja2-rust

Remove support of RGB distributions other than 565 (#28)
* State 2023-07-01 12:10:21

* State 2023-07-01 12:20:42

* State 2023-07-01 12:28:29

ja2lib/SGP/HImage.c
@@ -22,12 +22,6 @@
 #define BLACK_SUBSTITUTE 0x0001
 
 UINT16 gusAlphaMask = 0;
-UINT16 gusRedMask = 0;
-UINT16 gusGreenMask = 0;
-UINT16 gusBlueMask = 0;
-INT16 gusRedShift = 0;
-INT16 gusBlueShift = 0;
-INT16 gusGreenShift = 0;
 
 // this funky union is used for fast 16-bit pixel format conversions
 typedef union {
@@ -428,22 +422,11 @@ UINT16 *Create16BPPPalette(struct SGPPaletteEntry *pPalette) {
     g = pPalette[cnt].peGreen;
     b = pPalette[cnt].peBlue;
 
-    if (gusRedShift < 0)
-      r16 = ((UINT16)r >> abs(gusRedShift));
-    else
-      r16 = ((UINT16)r << gusRedShift);
+    r16 = ((UINT16)r << 8);
+    g16 = ((UINT16)g << 3);
+    b16 = ((UINT16)b >> 3);
 
-    if (gusGreenShift < 0)
-      g16 = ((UINT16)g >> abs(gusGreenShift));
-    else
-      g16 = ((UINT16)g << gusGreenShift);
-
-    if (gusBlueShift < 0)
-      b16 = ((UINT16)b >> abs(gusBlueShift));
-    else
-      b16 = ((UINT16)b << gusBlueShift);
-
-    usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+    usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
     if (usColor == 0) {
       if ((r + g + b) != 0) usColor = BLACK_SUBSTITUTE | gusAlphaMask;
@@ -507,23 +490,12 @@ UINT16 *Create16BPPPaletteShaded(struct SGPPaletteEntry *pPalette, UINT32 rscale
     g = (UINT8)min(gmod, 255);
     b = (UINT8)min(bmod, 255);
 
-    if (gusRedShift < 0)
-      r16 = ((UINT16)r >> (-gusRedShift));
-    else
-      r16 = ((UINT16)r << gusRedShift);
-
-    if (gusGreenShift < 0)
-      g16 = ((UINT16)g >> (-gusGreenShift));
-    else
-      g16 = ((UINT16)g << gusGreenShift);
-
-    if (gusBlueShift < 0)
-      b16 = ((UINT16)b >> (-gusBlueShift));
-    else
-      b16 = ((UINT16)b << gusBlueShift);
+    r16 = ((UINT16)r << 8);
+    g16 = ((UINT16)g << 3);
+    b16 = ((UINT16)b >> 3);
 
     // Prevent creation of pure black color
-    usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+    usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
     if (usColor == 0) {
       if ((r + g + b) != 0) usColor = BLACK_SUBSTITUTE | gusAlphaMask;
@@ -544,22 +516,11 @@ UINT16 Get16BPPColor(UINT32 RGBValue) {
   g = SGPGetGValue(RGBValue);
   b = SGPGetBValue(RGBValue);
 
-  if (gusRedShift < 0)
-    r16 = ((UINT16)r >> abs(gusRedShift));
-  else
-    r16 = ((UINT16)r << gusRedShift);
+  r16 = ((UINT16)r << 8);
+  g16 = ((UINT16)g << 3);
+  b16 = ((UINT16)b >> 3);
 
-  if (gusGreenShift < 0)
-    g16 = ((UINT16)g >> abs(gusGreenShift));
-  else
-    g16 = ((UINT16)g << gusGreenShift);
-
-  if (gusBlueShift < 0)
-    b16 = ((UINT16)b >> abs(gusBlueShift));
-  else
-    b16 = ((UINT16)b << gusBlueShift);
-
-  usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+  usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
   // if our color worked out to absolute black, and the original wasn't
   // absolute black, convert it to a VERY dark grey to avoid transparency
@@ -578,24 +539,15 @@ UINT32 GetRGBColor(UINT16 Value16BPP) {
   UINT16 r16, g16, b16;
   UINT32 r, g, b, val;
 
-  r16 = Value16BPP & gusRedMask;
-  g16 = Value16BPP & gusGreenMask;
-  b16 = Value16BPP & gusBlueMask;
+  r16 = Value16BPP & 0xf800;
+  g16 = Value16BPP & 0x07e0;
+  b16 = Value16BPP & 0x001f;
 
-  if (gusRedShift < 0)
-    r = ((UINT32)r16 << abs(gusRedShift));
-  else
-    r = ((UINT32)r16 >> gusRedShift);
+  r = ((UINT32)r16 >> 8);
 
-  if (gusGreenShift < 0)
-    g = ((UINT32)g16 << abs(gusGreenShift));
-  else
-    g = ((UINT32)g16 >> gusGreenShift);
+  g = ((UINT32)g16 >> 3);
 
-  if (gusBlueShift < 0)
-    b = ((UINT32)b16 << abs(gusBlueShift));
-  else
-    b = ((UINT32)b16 >> gusBlueShift);
+  b = ((UINT32)b16 << 3);
 
   r &= 0x000000ff;
   g &= 0x000000ff;
@@ -692,79 +644,4 @@ void ConvertRGBDistribution565To555(UINT16 *p16BPPData, UINT32 uiNumberOfPixels)
     }
     pPixel++;
   }
-}
-
-void ConvertRGBDistribution565To655(UINT16 *p16BPPData, UINT32 uiNumberOfPixels) {
-  UINT16 *pPixel;
-  UINT32 uiLoop;
-
-  SplitUINT32 Pixel;
-
-  pPixel = p16BPPData;
-  for (uiLoop = 0; uiLoop < uiNumberOfPixels; uiLoop++) {
-    // we put the 16 pixel bits in the UPPER word of uiPixel, so that we can
-    // right shift the blue value (at the bottom) into the LOWER word to protect it
-    Pixel.usHigher = *pPixel;
-    Pixel.uiValue >>= 5;
-    // get rid of the least significant bit of green
-    Pixel.usHigher >>= 1;
-    // shift to the right some more...
-    Pixel.uiValue >>= 5;
-    // so we can left-shift the red value alone to give it an extra bit
-    Pixel.usHigher <<= 1;
-    // now shift back and copy
-    Pixel.uiValue <<= 10;
-    *pPixel = Pixel.usHigher;
-    pPixel++;
-  }
-}
-
-void ConvertRGBDistribution565To556(UINT16 *p16BPPData, UINT32 uiNumberOfPixels) {
-  UINT16 *pPixel;
-  UINT32 uiLoop;
-
-  SplitUINT32 Pixel;
-
-  pPixel = p16BPPData;
-  for (uiLoop = 0; uiLoop < uiNumberOfPixels; uiLoop++) {
-    // we put the 16 pixel bits in the UPPER word of uiPixel, so that we can
-    // right shift the blue value (at the bottom) into the LOWER word to protect it
-    Pixel.usHigher = *pPixel;
-    Pixel.uiValue >>= 5;
-    // get rid of the least significant bit of green
-    Pixel.usHigher >>= 1;
-    // shift back into the upper word
-    Pixel.uiValue <<= 5;
-    // give blue an extra bit (blank in the least significant spot)
-    Pixel.usHigher <<= 1;
-    // copy back
-    *pPixel = Pixel.usHigher;
-    pPixel++;
-  }
-}
-
-void ConvertRGBDistribution565ToAny(UINT16 *p16BPPData, UINT32 uiNumberOfPixels) {
-  UINT16 *pPixel;
-  UINT32 uiRed, uiGreen, uiBlue, uiTemp, uiLoop;
-
-  pPixel = p16BPPData;
-  for (uiLoop = 0; uiLoop < uiNumberOfPixels; uiLoop++) {
-    // put the 565 RGB 16-bit value into a 32-bit RGB value
-    uiRed = (*pPixel) >> 11;
-    uiGreen = (*pPixel & 0x07E0) >> 5;
-    uiBlue = (*pPixel & 0x001F);
-    uiTemp = FROMRGB(uiRed, uiGreen, uiBlue);
-    // then convert the 32-bit RGB value to whatever 16 bit format is used
-    *pPixel = Get16BPPColor(uiTemp);
-    pPixel++;
-  }
-}
-
-BOOLEAN GetPrimaryRGBDistributionMasks(UINT32 *RedBitMask, UINT32 *GreenBitMask,
-                                       UINT32 *BlueBitMask) {
-  *RedBitMask = gusRedMask;
-  *GreenBitMask = gusGreenMask;
-  *BlueBitMask = gusBlueMask;
-
-  return TRUE;
 }

ja2lib/Tactical/SoldierControl.c
@@ -7717,23 +7717,12 @@ UINT16 *CreateEnemyGlow16BPPPalette(struct SGPPaletteEntry *pPalette, UINT32 rsc
     g = (UINT8)min(gmod, 255);
     b = (UINT8)min(bmod, 255);
 
-    if (gusRedShift < 0)
-      r16 = ((UINT16)r >> (-gusRedShift));
-    else
-      r16 = ((UINT16)r << gusRedShift);
-
-    if (gusGreenShift < 0)
-      g16 = ((UINT16)g >> (-gusGreenShift));
-    else
-      g16 = ((UINT16)g << gusGreenShift);
-
-    if (gusBlueShift < 0)
-      b16 = ((UINT16)b >> (-gusBlueShift));
-    else
-      b16 = ((UINT16)b << gusBlueShift);
+    r16 = ((UINT16)r << 8);
+    g16 = ((UINT16)g << 3);
+    b16 = ((UINT16)b >> 3);
 
     // Prevent creation of pure black color
-    usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+    usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
     if ((usColor == 0) && ((r + g + b) != 0)) usColor = 0x0001;
 
@@ -7770,23 +7759,12 @@ UINT16 *CreateEnemyGreyGlow16BPPPalette(struct SGPPaletteEntry *pPalette, UINT32
     g = (UINT8)min(gmod, 255);
     b = (UINT8)min(bmod, 255);
 
-    if (gusRedShift < 0)
-      r16 = ((UINT16)r >> (-gusRedShift));
-    else
-      r16 = ((UINT16)r << gusRedShift);
-
-    if (gusGreenShift < 0)
-      g16 = ((UINT16)g >> (-gusGreenShift));
-    else
-      g16 = ((UINT16)g << gusGreenShift);
-
-    if (gusBlueShift < 0)
-      b16 = ((UINT16)b >> (-gusBlueShift));
-    else
-      b16 = ((UINT16)b << gusBlueShift);
+    r16 = ((UINT16)r << 8);
+    g16 = ((UINT16)g << 3);
+    b16 = ((UINT16)b >> 3);
 
     // Prevent creation of pure black color
-    usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+    usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
     if ((usColor == 0) && ((r + g + b) != 0)) usColor = 0x0001;
 

platform-win32/Video_win.c
@@ -170,13 +170,6 @@ extern UINT32 guiDirtyRegionExCount;
 BOOLEAN gfPrintFrameBuffer;
 UINT32 guiPrintFrameBufferIndex;
 
-extern UINT16 gusRedMask;
-extern UINT16 gusGreenMask;
-extern UINT16 gusBlueMask;
-extern INT16 gusRedShift;
-extern INT16 gusBlueShift;
-extern INT16 gusGreenShift;
-
 BOOLEAN InitializeVideoManager(struct PlatformInitParams *params) {
   UINT32 uiIndex;
   HRESULT ReturnCode;
@@ -488,7 +481,9 @@ BOOLEAN InitializeVideoManager(struct PlatformInitParams *params) {
   // This function must be called to setup RGB information
   //
 
-  GetRGBDistribution();
+  if (!GetRGBDistribution()) {
+    return FALSE;
+  }
 
   // create video surfaces from DD surfaces
   vsPrimary = CreateVideoSurfaceFromDDSurface(gpPrimarySurface);
@@ -961,32 +956,22 @@ void printFramebuffer() {
     //
 
     // 5/6/5.. create buffer...
-    if (gusRedMask == 0xF800 && gusGreenMask == 0x07E0 && gusBlueMask == 0x001F) {
-      p16BPPData = (UINT16 *)MemAlloc(640 * 2);
-    }
+    p16BPPData = (UINT16 *)MemAlloc(640 * 2);
 
     for (INT32 iIndex = 479; iIndex >= 0; iIndex--) {
       // ATE: OK, fix this such that it converts pixel format to 5/5/5
       // if current settings are 5/6/5....
-      if (gusRedMask == 0xF800 && gusGreenMask == 0x07E0 && gusBlueMask == 0x001F) {
-        // Read into a buffer...
-        memcpy(p16BPPData, (((UINT8 *)SurfaceDescription.lpSurface) + (iIndex * 640 * 2)), 640 * 2);
+      // Read into a buffer...
+      memcpy(p16BPPData, (((UINT8 *)SurfaceDescription.lpSurface) + (iIndex * 640 * 2)), 640 * 2);
 
-        // Convert....
-        ConvertRGBDistribution565To555(p16BPPData, 640);
+      // Convert....
+      ConvertRGBDistribution565To555(p16BPPData, 640);
 
-        // Write
-        fwrite(p16BPPData, 640 * 2, 1, OutputFile);
-      } else {
-        fwrite((void *)(((UINT8 *)SurfaceDescription.lpSurface) + (iIndex * 640 * 2)), 640 * 2, 1,
-               OutputFile);
-      }
+      // Write
+      fwrite(p16BPPData, 640 * 2, 1, OutputFile);
     }
 
-    // 5/6/5.. Delete buffer...
-    if (gusRedMask == 0xF800 && gusGreenMask == 0x07E0 && gusBlueMask == 0x001F) {
-      MemFree(p16BPPData);
-    }
+    MemFree(p16BPPData);
 
     fclose(OutputFile);
 
@@ -1402,37 +1387,26 @@ static BOOLEAN GetRGBDistribution(void) {
   // Ok we now have the surface description, we now can get the information that we need
   //
 
-  gusRedMask = (UINT16)SurfaceDescription.ddpfPixelFormat.dwRBitMask;
-  gusGreenMask = (UINT16)SurfaceDescription.ddpfPixelFormat.dwGBitMask;
-  gusBlueMask = (UINT16)SurfaceDescription.ddpfPixelFormat.dwBBitMask;
+  u16 usRedMask = (UINT16)SurfaceDescription.ddpfPixelFormat.dwRBitMask;
+  u16 usGreenMask = (UINT16)SurfaceDescription.ddpfPixelFormat.dwGBitMask;
+  u16 usBlueMask = (UINT16)SurfaceDescription.ddpfPixelFormat.dwBBitMask;
 
-  // RGB 5,5,5
-  if ((gusRedMask == 0x7c00) && (gusGreenMask == 0x03e0) && (gusBlueMask == 0x1f))
-    guiTranslucentMask = 0x3def;
-  // RGB 5,6,5
-  else  // if((gusRedMask==0xf800) && (gusGreenMask==0x03e0) && (gusBlueMask==0x1f))
-    guiTranslucentMask = 0x7bef;
-
-  usBit = 0x8000;
-  gusRedShift = 8;
-  while (!(gusRedMask & usBit)) {
-    usBit >>= 1;
-    gusRedShift--;
+  if ((usRedMask != 0xf800) || (usGreenMask != 0x07e0) || (usBlueMask != 0x001f)) {
+    char buf[200];
+    snprintf(buf, ARR_SIZE(buf), "XXX RGB distribution: (0x%04x, 0x%04x, 0x%04x)", usRedMask,
+             usGreenMask, usBlueMask);
+    DebugLogWrite(buf);
+    DebugLogWrite("XXX RGB distribution other than 565 is not supported");
+    // It may not work some hardware, but 16 bit mode is outdated anyway.
+    // We should switch to 32bit mode.
+    //
+    // Maybe useful:
+    //   - https://www.gamedev.net/forums/topic/54104-555-or-565/
+    //   - https://learn.microsoft.com/en-us/windows/win32/directshow/working-with-16-bit-rgb
+    return FALSE;
   }
 
-  usBit = 0x8000;
-  gusGreenShift = 8;
-  while (!(gusGreenMask & usBit)) {
-    usBit >>= 1;
-    gusGreenShift--;
-  }
-
-  usBit = 0x8000;
-  gusBlueShift = 8;
-  while (!(gusBlueMask & usBit)) {
-    usBit >>= 1;
-    gusBlueShift--;
-  }
+  guiTranslucentMask = 0x7bef;
 
   return TRUE;
 }
@@ -1512,17 +1486,11 @@ struct VSurface *CreateVideoSurface(u16 width, u16 height, u8 bitDepth) {
       break;
 
     case 16: {
-      UINT32 uiRBitMask;
-      UINT32 uiGBitMask;
-      UINT32 uiBBitMask;
       PixelFormat.dwFlags = DDPF_RGB;
       PixelFormat.dwRGBBitCount = 16;
-      if (!(GetPrimaryRGBDistributionMasks(&uiRBitMask, &uiGBitMask, &uiBBitMask))) {
-        return FALSE;
-      }
-      PixelFormat.dwRBitMask = uiRBitMask;
-      PixelFormat.dwGBitMask = uiGBitMask;
-      PixelFormat.dwBBitMask = uiBBitMask;
+      PixelFormat.dwRBitMask = 0xf800;
+      PixelFormat.dwGBitMask = 0x07e0;
+      PixelFormat.dwBBitMask = 0x001f;
     } break;
 
     default:

gtrafimenkov/ja2-experiments

ja2lib/SGP/HImage.c
@@ -26,12 +26,6 @@
 #define BLACK_SUBSTITUTE 0x0001
 
 uint16_t gusAlphaMask = 0;
-uint16_t gusRedMask = 0;
-uint16_t gusGreenMask = 0;
-uint16_t gusBlueMask = 0;
-int16_t gusRedShift = 0;
-int16_t gusBlueShift = 0;
-int16_t gusGreenShift = 0;
 
 // this funky union is used for fast 16-bit pixel format conversions
 typedef union {
@@ -435,22 +429,11 @@ uint16_t *Create16BPPPalette(struct SGPPaletteEntry *pPalette) {
     g = pPalette[cnt].peGreen;
     b = pPalette[cnt].peBlue;
 
-    if (gusRedShift < 0)
-      r16 = ((uint16_t)r >> abs(gusRedShift));
-    else
-      r16 = ((uint16_t)r << gusRedShift);
+    r16 = ((uint16_t)r << 8);
+    g16 = ((uint16_t)g << 3);
+    b16 = ((uint16_t)b >> 3);
 
-    if (gusGreenShift < 0)
-      g16 = ((uint16_t)g >> abs(gusGreenShift));
-    else
-      g16 = ((uint16_t)g << gusGreenShift);
-
-    if (gusBlueShift < 0)
-      b16 = ((uint16_t)b >> abs(gusBlueShift));
-    else
-      b16 = ((uint16_t)b << gusBlueShift);
-
-    usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+    usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
     if (usColor == 0) {
       if ((r + g + b) != 0) usColor = BLACK_SUBSTITUTE | gusAlphaMask;
@@ -514,23 +497,12 @@ uint16_t *Create16BPPPaletteShaded(struct SGPPaletteEntry *pPalette, uint32_t rs
     g = (uint8_t)min(gmod, 255);
     b = (uint8_t)min(bmod, 255);
 
-    if (gusRedShift < 0)
-      r16 = ((uint16_t)r >> (-gusRedShift));
-    else
-      r16 = ((uint16_t)r << gusRedShift);
-
-    if (gusGreenShift < 0)
-      g16 = ((uint16_t)g >> (-gusGreenShift));
-    else
-      g16 = ((uint16_t)g << gusGreenShift);
-
-    if (gusBlueShift < 0)
-      b16 = ((uint16_t)b >> (-gusBlueShift));
-    else
-      b16 = ((uint16_t)b << gusBlueShift);
+    r16 = ((uint16_t)r << 8);
+    g16 = ((uint16_t)g << 3);
+    b16 = ((uint16_t)b >> 3);
 
     // Prevent creation of pure black color
-    usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+    usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
     if (usColor == 0) {
       if ((r + g + b) != 0) usColor = BLACK_SUBSTITUTE | gusAlphaMask;
@@ -551,22 +523,11 @@ uint16_t Get16BPPColor(uint32_t RGBValue) {
   g = SGPGetGValue(RGBValue);
   b = SGPGetBValue(RGBValue);
 
-  if (gusRedShift < 0)
-    r16 = ((uint16_t)r >> abs(gusRedShift));
-  else
-    r16 = ((uint16_t)r << gusRedShift);
+  r16 = ((uint16_t)r << 8);
+  g16 = ((uint16_t)g << 3);
+  b16 = ((uint16_t)b >> 3);
 
-  if (gusGreenShift < 0)
-    g16 = ((uint16_t)g >> abs(gusGreenShift));
-  else
-    g16 = ((uint16_t)g << gusGreenShift);
-
-  if (gusBlueShift < 0)
-    b16 = ((uint16_t)b >> abs(gusBlueShift));
-  else
-    b16 = ((uint16_t)b << gusBlueShift);
-
-  usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+  usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
   // if our color worked out to absolute black, and the original wasn't
   // absolute black, convert it to a VERY dark grey to avoid transparency
@@ -585,24 +546,15 @@ uint32_t GetRGBColor(uint16_t Value16BPP) {
   uint16_t r16, g16, b16;
   uint32_t r, g, b, val;
 
-  r16 = Value16BPP & gusRedMask;
-  g16 = Value16BPP & gusGreenMask;
-  b16 = Value16BPP & gusBlueMask;
+  r16 = Value16BPP & 0xf800;
+  g16 = Value16BPP & 0x07e0;
+  b16 = Value16BPP & 0x001f;
 
-  if (gusRedShift < 0)
-    r = ((uint32_t)r16 << abs(gusRedShift));
-  else
-    r = ((uint32_t)r16 >> gusRedShift);
+  r = ((uint32_t)r16 >> 8);
 
-  if (gusGreenShift < 0)
-    g = ((uint32_t)g16 << abs(gusGreenShift));
-  else
-    g = ((uint32_t)g16 >> gusGreenShift);
+  g = ((uint32_t)g16 >> 3);
 
-  if (gusBlueShift < 0)
-    b = ((uint32_t)b16 << abs(gusBlueShift));
-  else
-    b = ((uint32_t)b16 >> gusBlueShift);
+  b = ((uint32_t)b16 << 3);
 
   r &= 0x000000ff;
   g &= 0x000000ff;
@@ -700,79 +652,4 @@ void ConvertRGBDistribution565To555(uint16_t *p16BPPData, uint32_t uiNumberOfPix
     }
     pPixel++;
   }
-}
-
-void ConvertRGBDistribution565To655(uint16_t *p16BPPData, uint32_t uiNumberOfPixels) {
-  uint16_t *pPixel;
-  uint32_t uiLoop;
-
-  SplitUINT32 Pixel;
-
-  pPixel = p16BPPData;
-  for (uiLoop = 0; uiLoop < uiNumberOfPixels; uiLoop++) {
-    // we put the 16 pixel bits in the UPPER word of uiPixel, so that we can
-    // right shift the blue value (at the bottom) into the LOWER word to protect it
-    Pixel.usHigher = *pPixel;
-    Pixel.uiValue >>= 5;
-    // get rid of the least significant bit of green
-    Pixel.usHigher >>= 1;
-    // shift to the right some more...
-    Pixel.uiValue >>= 5;
-    // so we can left-shift the red value alone to give it an extra bit
-    Pixel.usHigher <<= 1;
-    // now shift back and copy
-    Pixel.uiValue <<= 10;
-    *pPixel = Pixel.usHigher;
-    pPixel++;
-  }
-}
-
-void ConvertRGBDistribution565To556(uint16_t *p16BPPData, uint32_t uiNumberOfPixels) {
-  uint16_t *pPixel;
-  uint32_t uiLoop;
-
-  SplitUINT32 Pixel;
-
-  pPixel = p16BPPData;
-  for (uiLoop = 0; uiLoop < uiNumberOfPixels; uiLoop++) {
-    // we put the 16 pixel bits in the UPPER word of uiPixel, so that we can
-    // right shift the blue value (at the bottom) into the LOWER word to protect it
-    Pixel.usHigher = *pPixel;
-    Pixel.uiValue >>= 5;
-    // get rid of the least significant bit of green
-    Pixel.usHigher >>= 1;
-    // shift back into the upper word
-    Pixel.uiValue <<= 5;
-    // give blue an extra bit (blank in the least significant spot)
-    Pixel.usHigher <<= 1;
-    // copy back
-    *pPixel = Pixel.usHigher;
-    pPixel++;
-  }
-}
-
-void ConvertRGBDistribution565ToAny(uint16_t *p16BPPData, uint32_t uiNumberOfPixels) {
-  uint16_t *pPixel;
-  uint32_t uiRed, uiGreen, uiBlue, uiTemp, uiLoop;
-
-  pPixel = p16BPPData;
-  for (uiLoop = 0; uiLoop < uiNumberOfPixels; uiLoop++) {
-    // put the 565 RGB 16-bit value into a 32-bit RGB value
-    uiRed = (*pPixel) >> 11;
-    uiGreen = (*pPixel & 0x07E0) >> 5;
-    uiBlue = (*pPixel & 0x001F);
-    uiTemp = FROMRGB(uiRed, uiGreen, uiBlue);
-    // then convert the 32-bit RGB value to whatever 16 bit format is used
-    *pPixel = Get16BPPColor(uiTemp);
-    pPixel++;
-  }
-}
-
-BOOLEAN GetPrimaryRGBDistributionMasks(uint32_t *RedBitMask, uint32_t *GreenBitMask,
-                                       uint32_t *BlueBitMask) {
-  *RedBitMask = gusRedMask;
-  *GreenBitMask = gusGreenMask;
-  *BlueBitMask = gusBlueMask;
-
-  return TRUE;
 }

ja2lib/Tactical/SoldierControl.c
@@ -7726,23 +7726,12 @@ uint16_t *CreateEnemyGlow16BPPPalette(struct SGPPaletteEntry *pPalette, uint32_t
     g = (uint8_t)min(gmod, 255);
     b = (uint8_t)min(bmod, 255);
 
-    if (gusRedShift < 0)
-      r16 = ((uint16_t)r >> (-gusRedShift));
-    else
-      r16 = ((uint16_t)r << gusRedShift);
-
-    if (gusGreenShift < 0)
-      g16 = ((uint16_t)g >> (-gusGreenShift));
-    else
-      g16 = ((uint16_t)g << gusGreenShift);
-
-    if (gusBlueShift < 0)
-      b16 = ((uint16_t)b >> (-gusBlueShift));
-    else
-      b16 = ((uint16_t)b << gusBlueShift);
+    r16 = ((uint16_t)r << 8);
+    g16 = ((uint16_t)g << 3);
+    b16 = ((uint16_t)b >> 3);
 
     // Prevent creation of pure black color
-    usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+    usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
     if ((usColor == 0) && ((r + g + b) != 0)) usColor = 0x0001;
 
@@ -7779,23 +7768,12 @@ uint16_t *CreateEnemyGreyGlow16BPPPalette(struct SGPPaletteEntry *pPalette, uint
     g = (uint8_t)min(gmod, 255);
     b = (uint8_t)min(bmod, 255);
 
-    if (gusRedShift < 0)
-      r16 = ((uint16_t)r >> (-gusRedShift));
-    else
-      r16 = ((uint16_t)r << gusRedShift);
-
-    if (gusGreenShift < 0)
-      g16 = ((uint16_t)g >> (-gusGreenShift));
-    else
-      g16 = ((uint16_t)g << gusGreenShift);
-
-    if (gusBlueShift < 0)
-      b16 = ((uint16_t)b >> (-gusBlueShift));
-    else
-      b16 = ((uint16_t)b << gusBlueShift);
+    r16 = ((uint16_t)r << 8);
+    g16 = ((uint16_t)g << 3);
+    b16 = ((uint16_t)b >> 3);
 
     // Prevent creation of pure black color
-    usColor = (r16 & gusRedMask) | (g16 & gusGreenMask) | (b16 & gusBlueMask);
+    usColor = (r16 & 0xf800) | (g16 & 0x07e0) | (b16 & 0x001f);
 
     if ((usColor == 0) && ((r + g + b) != 0)) usColor = 0x0001;
 

platform-win32/Video_win.c
@@ -175,13 +175,6 @@ extern uint32_t guiDirtyRegionExCount;
 BOOLEAN gfPrintFrameBuffer;
 uint32_t guiPrintFrameBufferIndex;
 
-extern uint16_t gusRedMask;
-extern uint16_t gusGreenMask;
-extern uint16_t gusBlueMask;
-extern int16_t gusRedShift;
-extern int16_t gusBlueShift;
-extern int16_t gusGreenShift;
-
 BOOLEAN InitializeVideoManager(struct PlatformInitParams *params) {
   uint32_t uiIndex;
   HRESULT ReturnCode;
@@ -493,7 +486,9 @@ BOOLEAN InitializeVideoManager(struct PlatformInitParams *params) {
   // This function must be called to setup RGB information
   //
 
-  GetRGBDistribution();
+  if (!GetRGBDistribution()) {
+    return FALSE;
+  }
 
   // create video surfaces from DD surfaces
   vsPrimary = CreateVideoSurfaceFromDDSurface(gpPrimarySurface);
@@ -967,33 +962,22 @@ void printFramebuffer() {
     //
 
     // 5/6/5.. create buffer...
-    if (gusRedMask == 0xF800 && gusGreenMask == 0x07E0 && gusBlueMask == 0x001F) {
-      p16BPPData = (uint16_t *)MemAlloc(640 * 2);
-    }
+    p16BPPData = (uint16_t *)MemAlloc(640 * 2);
 
     for (int32_t iIndex = 479; iIndex >= 0; iIndex--) {
       // ATE: OK, fix this such that it converts pixel format to 5/5/5
       // if current settings are 5/6/5....
-      if (gusRedMask == 0xF800 && gusGreenMask == 0x07E0 && gusBlueMask == 0x001F) {
-        // Read into a buffer...
-        memcpy(p16BPPData, (((uint8_t *)SurfaceDescription.lpSurface) + (iIndex * 640 * 2)),
-               640 * 2);
+      // Read into a buffer...
+      memcpy(p16BPPData, (((UINT8 *)SurfaceDescription.lpSurface) + (iIndex * 640 * 2)), 640 * 2);
 
-        // Convert....
-        ConvertRGBDistribution565To555(p16BPPData, 640);
+      // Convert....
+      ConvertRGBDistribution565To555(p16BPPData, 640);
 
-        // Write
-        fwrite(p16BPPData, 640 * 2, 1, OutputFile);
-      } else {
-        fwrite((void *)(((uint8_t *)SurfaceDescription.lpSurface) + (iIndex * 640 * 2)), 640 * 2, 1,
-               OutputFile);
-      }
+      // Write
+      fwrite(p16BPPData, 640 * 2, 1, OutputFile);
     }
 
-    // 5/6/5.. Delete buffer...
-    if (gusRedMask == 0xF800 && gusGreenMask == 0x07E0 && gusBlueMask == 0x001F) {
-      MemFree(p16BPPData);
-    }
+    MemFree(p16BPPData);
 
     fclose(OutputFile);
 
@@ -1409,37 +1393,26 @@ static BOOLEAN GetRGBDistribution(void) {
   // Ok we now have the surface description, we now can get the information that we need
   //
 
-  gusRedMask = (uint16_t)SurfaceDescription.ddpfPixelFormat.dwRBitMask;
-  gusGreenMask = (uint16_t)SurfaceDescription.ddpfPixelFormat.dwGBitMask;
-  gusBlueMask = (uint16_t)SurfaceDescription.ddpfPixelFormat.dwBBitMask;
+  uint16_t usRedMask = (uint16_t)SurfaceDescription.ddpfPixelFormat.dwRBitMask;
+  uint16_t usGreenMask = (uint16_t)SurfaceDescription.ddpfPixelFormat.dwGBitMask;
+  uint16_t usBlueMask = (uint16_t)SurfaceDescription.ddpfPixelFormat.dwBBitMask;
 
-  // RGB 5,5,5
-  if ((gusRedMask == 0x7c00) && (gusGreenMask == 0x03e0) && (gusBlueMask == 0x1f))
-    guiTranslucentMask = 0x3def;
-  // RGB 5,6,5
-  else  // if((gusRedMask==0xf800) && (gusGreenMask==0x03e0) && (gusBlueMask==0x1f))
-    guiTranslucentMask = 0x7bef;
-
-  usBit = 0x8000;
-  gusRedShift = 8;
-  while (!(gusRedMask & usBit)) {
-    usBit >>= 1;
-    gusRedShift--;
+  if ((usRedMask != 0xf800) || (usGreenMask != 0x07e0) || (usBlueMask != 0x001f)) {
+    char buf[200];
+    snprintf(buf, ARR_SIZE(buf), "XXX RGB distribution: (0x%04x, 0x%04x, 0x%04x)", usRedMask,
+             usGreenMask, usBlueMask);
+    DebugLogWrite(buf);
+    DebugLogWrite("XXX RGB distribution other than 565 is not supported");
+    // It may not work some hardware, but 16 bit mode is outdated anyway.
+    // We should switch to 32bit mode.
+    //
+    // Maybe useful:
+    //   - https://www.gamedev.net/forums/topic/54104-555-or-565/
+    //   - https://learn.microsoft.com/en-us/windows/win32/directshow/working-with-16-bit-rgb
+    return FALSE;
   }
 
-  usBit = 0x8000;
-  gusGreenShift = 8;
-  while (!(gusGreenMask & usBit)) {
-    usBit >>= 1;
-    gusGreenShift--;
-  }
-
-  usBit = 0x8000;
-  gusBlueShift = 8;
-  while (!(gusBlueMask & usBit)) {
-    usBit >>= 1;
-    gusBlueShift--;
-  }
+  guiTranslucentMask = 0x7bef;
 
   return TRUE;
 }
@@ -1519,17 +1492,11 @@ struct VSurface *CreateVideoSurface(uint16_t width, uint16_t height, uint8_t bit
       break;
 
     case 16: {
-      uint32_t uiRBitMask;
-      uint32_t uiGBitMask;
-      uint32_t uiBBitMask;
       PixelFormat.dwFlags = DDPF_RGB;
       PixelFormat.dwRGBBitCount = 16;
-      if (!(GetPrimaryRGBDistributionMasks(&uiRBitMask, &uiGBitMask, &uiBBitMask))) {
-        return FALSE;
-      }
-      PixelFormat.dwRBitMask = uiRBitMask;
-      PixelFormat.dwGBitMask = uiGBitMask;
-      PixelFormat.dwBBitMask = uiBBitMask;
+      PixelFormat.dwRBitMask = 0xf800;
+      PixelFormat.dwGBitMask = 0x07e0;
+      PixelFormat.dwBBitMask = 0x001f;
     } break;
 
     default: