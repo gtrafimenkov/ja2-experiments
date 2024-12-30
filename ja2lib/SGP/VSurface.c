gtrafimenkov/ja2-rust

Restore broken video call distortion
It was broken in 67da271bf3dd5cde9da14d6c2d7159cdca66b5f3

ja2lib/SGP/VSurface.c
@@ -404,6 +404,7 @@ static uint32_t addVSurfaceToList(struct VSurface *vs) {
 struct VSurface *VSurfaceAdd(u16 width, u16 height, VSurfID *puiIndex) {
   struct VSurface *vs = CreateVideoSurface(width, height);
   if (vs) {
+    SetVideoSurfaceTransparencyColor(vs, FROMRGB(0, 0, 0));
     if (puiIndex) {
       *puiIndex = addVSurfaceToList(vs);
     }
@@ -422,6 +423,7 @@ BOOLEAN AddVideoSurface(VSURFACE_DESC *desc, VSurfID *puiIndex) {
     return FALSE;
   }
 
+  SetVideoSurfaceTransparencyColor(vs, FROMRGB(0, 0, 0));
   *puiIndex = addVSurfaceToList(vs);
   return TRUE;
 }
@@ -431,6 +433,26 @@ BYTE *VSurfaceLockOld(struct VSurface *vs, u32 *pitch) {
   struct BufferLockInfo res = VSurfaceLock(vs);
   *pitch = res.pitch;
   return res.dest;
+}
+
+BOOLEAN SetVideoSurfaceTransparency(UINT32 uiIndex, COLORVAL TransColor) {
+  struct VSurface *hVSurface;
+
+  //
+  // Get Video Surface
+  //
+
+  if (!(GetVideoSurface(&hVSurface, uiIndex))) {
+    return FALSE;
+  }
+
+  //
+  // Set transparency
+  //
+
+  SetVideoSurfaceTransparencyColor(hVSurface, TransColor);
+
+  return (TRUE);
 }
 
 struct VSurface *GetVSByID(VSurfID id) {

ja2lib/SGP/VSurface.h
@@ -77,6 +77,9 @@ struct VSurface *GetVSByID(VSurfID id);
 
 BOOLEAN VSurfaceColorFill(struct VSurface *dest, i32 x1, i32 y1, i32 x2, i32 y2, u16 Color16BPP);
 
+// Sets transparency
+BOOLEAN SetVideoSurfaceTransparency(UINT32 uiIndex, COLORVAL TransColor);
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // Video Surface manipulation functions
@@ -86,6 +89,9 @@ BOOLEAN VSurfaceColorFill(struct VSurface *dest, i32 x1, i32 y1, i32 x2, i32 y2,
 // Set data from struct Image*.
 BOOLEAN SetVideoSurfaceDataFromHImage(struct VSurface *hVSurface, struct Image *hImage, UINT16 usX,
                                       UINT16 usY, struct GRect *pSrcRect);
+
+// Sets Transparency color into HVSurface and the underlying DD surface
+BOOLEAN SetVideoSurfaceTransparencyColor(struct VSurface *hVSurface, COLORVAL TransColor);
 
 // Deletes all data, including palettes, regions, DD Surfaces
 BOOLEAN DeleteVideoSurface(struct VSurface *hVSurface);

gtrafimenkov/ja2-experiments

ja2lib/SGP/VSurface.c
@@ -406,6 +406,7 @@ static uint32_t addVSurfaceToList(struct VSurface *vs) {
 struct VSurface *VSurfaceAdd(uint16_t width, uint16_t height, VSurfID *puiIndex) {
   struct VSurface *vs = CreateVideoSurface(width, height);
   if (vs) {
+    SetVideoSurfaceTransparencyColor(vs, FROMRGB(0, 0, 0));
     if (puiIndex) {
       *puiIndex = addVSurfaceToList(vs);
     }
@@ -424,6 +425,7 @@ BOOLEAN AddVideoSurface(VSURFACE_DESC *desc, VSurfID *puiIndex) {
     return FALSE;
   }
 
+  SetVideoSurfaceTransparencyColor(vs, FROMRGB(0, 0, 0));
   *puiIndex = addVSurfaceToList(vs);
   return TRUE;
 }
@@ -433,6 +435,26 @@ BYTE *VSurfaceLockOld(struct VSurface *vs, uint32_t *pitch) {
   struct BufferLockInfo res = VSurfaceLock(vs);
   *pitch = res.pitch;
   return res.dest;
+}
+
+BOOLEAN SetVideoSurfaceTransparency(uint32_t uiIndex, COLORVAL TransColor) {
+  struct VSurface *hVSurface;
+
+  //
+  // Get Video Surface
+  //
+
+  if (!(GetVideoSurface(&hVSurface, uiIndex))) {
+    return FALSE;
+  }
+
+  //
+  // Set transparency
+  //
+
+  SetVideoSurfaceTransparencyColor(hVSurface, TransColor);
+
+  return (TRUE);
 }
 
 struct VSurface *GetVSByID(VSurfID id) {

ja2lib/SGP/VSurface.h
@@ -82,6 +82,9 @@ struct VSurface *GetVSByID(VSurfID id);
 BOOLEAN VSurfaceColorFill(struct VSurface *dest, int32_t x1, int32_t y1, int32_t x2, int32_t y2,
                           uint16_t Color16BPP);
 
+// Sets transparency
+BOOLEAN SetVideoSurfaceTransparency(uint32_t uiIndex, COLORVAL TransColor);
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // Video Surface manipulation functions
@@ -91,6 +94,9 @@ BOOLEAN VSurfaceColorFill(struct VSurface *dest, int32_t x1, int32_t y1, int32_t
 // Set data from struct Image*.
 BOOLEAN SetVideoSurfaceDataFromHImage(struct VSurface *hVSurface, struct Image *hImage,
                                       uint16_t usX, uint16_t usY, struct GRect *pSrcRect);
+
+// Sets Transparency color into HVSurface and the underlying DD surface
+BOOLEAN SetVideoSurfaceTransparencyColor(struct VSurface *hVSurface, COLORVAL TransColor);
 
 // Deletes all data, including palettes, regions, DD Surfaces
 BOOLEAN DeleteVideoSurface(struct VSurface *hVSurface);