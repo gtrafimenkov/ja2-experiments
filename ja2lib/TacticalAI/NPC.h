gtrafimenkov/ja2-rust

Implement FileMan operations in rust (#12)
* Implement FileMan operations in rust

* Bugfixes

* Cleaner interface with FileMan_OpenForWriting

* Cleaner interface with FileMan_OpenForAppending

* Cleaning up FileMan_Open interface

* Create cleaner interface with FileMan_OpenForReading

* Create cleaner interface withh FileMan_OpenForReading

* Create cleaner interface withh FileMan_OpenForReading

* Replace open for rw code

* Mark functions that need reimplementation

* Remove FileMan_ExistsNoDB

* Replace FileMan_Size

* Remove debug code that uses FileMan_GetPos

* Remove FileMan_GetPos

* Remove FileMan_CheckEndOfFile

* Solve parallel read problem

* Remove some autosave functionality and FileMan_GetFileWriteTime

* Replace FileMan functions in ja2lib

* A few of FileMan functions needed only in platform-win32 code

Sound and Video streaming

* Support video playback using new file functions

* Support sound playback using new file functions

* Remove debug logging

* Remove old FileMan functions

* Remove no longer needed code

* Fix compilation on Linux

ja2lib/TacticalAI/NPC.h
@@ -160,8 +160,8 @@ extern BOOLEAN ReloadQuoteFile(UINT8 ubNPC);
 extern BOOLEAN ReloadAllQuoteFiles(void);
 
 // Save and loads the npc info to a saved game file
-extern BOOLEAN SaveNPCInfoToSaveGameFile(HWFILE hFile);
-BOOLEAN LoadNPCInfoFromSavedGameFile(HWFILE hFile, UINT32 uiSaveGameVersion);
+extern BOOLEAN SaveNPCInfoToSaveGameFile(FileID hFile);
+BOOLEAN LoadNPCInfoFromSavedGameFile(FileID hFile, UINT32 uiSaveGameVersion);
 
 extern void TriggerFriendWithHostileQuote(UINT8 ubNPC);
 
@@ -185,8 +185,8 @@ BOOLEAN HandleShopKeepHasBeenShutDown(UINT8 ubCharNum);
 BOOLEAN NPCHasUnusedRecordWithGivenApproach(UINT8 ubNPC, UINT8 ubApproach);
 BOOLEAN NPCWillingToAcceptItem(UINT8 ubNPC, UINT8 ubMerc, struct OBJECTTYPE *pObj);
 
-BOOLEAN SaveBackupNPCInfoToSaveGameFile(HWFILE hFile);
-BOOLEAN LoadBackupNPCInfoFromSavedGameFile(HWFILE hFile, UINT32 uiSaveGameVersion);
+BOOLEAN SaveBackupNPCInfoToSaveGameFile(FileID hFile);
+BOOLEAN LoadBackupNPCInfoFromSavedGameFile(FileID hFile, UINT32 uiSaveGameVersion);
 
 #ifdef JA2BETAVERSION
 void ToggleNPCRecordDisplay(void);

gtrafimenkov/ja2-experiments

ja2lib/TacticalAI/NPC.h
@@ -164,8 +164,8 @@ extern BOOLEAN ReloadQuoteFile(uint8_t ubNPC);
 extern BOOLEAN ReloadAllQuoteFiles(void);
 
 // Save and loads the npc info to a saved game file
-extern BOOLEAN SaveNPCInfoToSaveGameFile(HWFILE hFile);
-BOOLEAN LoadNPCInfoFromSavedGameFile(HWFILE hFile, uint32_t uiSaveGameVersion);
+extern BOOLEAN SaveNPCInfoToSaveGameFile(FileID hFile);
+BOOLEAN LoadNPCInfoFromSavedGameFile(FileID hFile, uint32_t uiSaveGameVersion);
 
 extern void TriggerFriendWithHostileQuote(uint8_t ubNPC);
 
@@ -190,8 +190,8 @@ BOOLEAN HandleShopKeepHasBeenShutDown(uint8_t ubCharNum);
 BOOLEAN NPCHasUnusedRecordWithGivenApproach(uint8_t ubNPC, uint8_t ubApproach);
 BOOLEAN NPCWillingToAcceptItem(uint8_t ubNPC, uint8_t ubMerc, struct OBJECTTYPE *pObj);
 
-BOOLEAN SaveBackupNPCInfoToSaveGameFile(HWFILE hFile);
-BOOLEAN LoadBackupNPCInfoFromSavedGameFile(HWFILE hFile, uint32_t uiSaveGameVersion);
+BOOLEAN SaveBackupNPCInfoToSaveGameFile(FileID hFile);
+BOOLEAN LoadBackupNPCInfoFromSavedGameFile(FileID hFile, uint32_t uiSaveGameVersion);
 
 #ifdef JA2BETAVERSION
 void ToggleNPCRecordDisplay(void);