gtrafimenkov/ja2-rust

Stop using 8bit video surfaces (#34)
* Implement Blt8BPPDataTo16BPPBufferHalf in C

* Improvement

* Add Blt16BPPDataTo16BPPScaleDown2x

* Replacing Blt8BPPDataSubTo16BPPBuffer with BlitSurfaceToSurface

BlitSurfaceToSurface is more universal - it checks for the source vsurface bit depth.

* remove unused zoom functionality

* remove unused zoom functionality

* remove unused zoom functionality

* remove unused zoom functionality

* remove unused zoom functionality

* remove unused zoom functionality

* remove unused zoom functionality

* remove unused zoom functionality

* remove unused zoom functionality

* Simplify map graphics preload

* State 2023-07-04 07:42:52

* State 2023-07-04 07:56:40

* State 2023-07-04 08:56:04

* State 2023-07-04 09:06:44

* State 2023-07-04 14:46:37

* State 2023-07-04 15:02:09

* State 2023-07-04 15:06:43

* State 2023-07-04 20:05:07

* State 2023-07-04 21:12:35

* State 2023-07-05 06:36:43

* move SGPRect to rust

* move SGPRect to rust

* move Rect to rust

* pass GRect instead of 4 params

* State 2023-07-05 07:47:19

* State 2023-07-05 08:32:21

* State 2023-07-05 08:36:41

* State 2023-07-05 08:44:12

* State 2023-07-05 11:04:17

* State 2023-07-05 11:18:18

* State 2023-07-05 11:29:51

* State 2023-07-05 11:31:12

* State 2023-07-05 11:31:51

* State 2023-07-05 13:58:11

* State 2023-07-05 14:02:20

* remove correction code for old broken saves

* State 2023-07-05 16:43:30

* State 2023-07-05 16:58:27

* State 2023-07-05 17:34:32

* State 2023-07-05 17:37:48

* State 2023-07-05 17:40:48

* VSurface only 16 bit now

* State 2023-07-05 18:03:02

* State 2023-07-05 18:17:16

* State 2023-07-05 18:29:27

* State 2023-07-05 18:35:40

* State 2023-07-05 18:36:22

* State 2023-07-05 19:42:00

* State 2023-07-05 19:43:51

* State 2023-07-05 19:44:59

* State 2023-07-05 19:46:01

* State 2023-07-05 19:47:29

* State 2023-07-

ja2lib/TileEngine/RenderFun.c
@@ -37,7 +37,7 @@ void SetTileRoomNum(INT16 sGridNo, UINT8 ubRoomNum) {
   gubWorldRoomInfo[sGridNo] = ubRoomNum;
 }
 
-void SetTileRangeRoomNum(SGPRect *pSelectRegion, UINT8 ubRoomNum) {
+void SetTileRangeRoomNum(struct GRect *pSelectRegion, UINT8 ubRoomNum) {
   INT32 cnt1, cnt2;
 
   for (cnt1 = pSelectRegion->iTop; cnt1 <= pSelectRegion->iBottom; cnt1++) {
@@ -255,7 +255,7 @@ void RemoveRoomRoof(UINT16 sGridNo, UINT8 bRoomNum, struct SOLDIERTYPE *pSoldier
   CalculateWorldWireFrameTiles(FALSE);
 }
 
-BOOLEAN AddSpecialTileRange(SGPRect *pSelectRegion) {
+BOOLEAN AddSpecialTileRange(struct GRect *pSelectRegion) {
   INT32 cnt1, cnt2;
 
   for (cnt1 = pSelectRegion->iTop; cnt1 <= pSelectRegion->iBottom; cnt1++) {
@@ -267,7 +267,7 @@ BOOLEAN AddSpecialTileRange(SGPRect *pSelectRegion) {
   return (TRUE);
 }
 
-BOOLEAN RemoveSpecialTileRange(SGPRect *pSelectRegion) {
+BOOLEAN RemoveSpecialTileRange(struct GRect *pSelectRegion) {
   INT32 cnt1, cnt2;
 
   for (cnt1 = pSelectRegion->iTop; cnt1 <= pSelectRegion->iBottom; cnt1++) {

gtrafimenkov/ja2-experiments

ja2lib/TileEngine/RenderFun.c
@@ -41,7 +41,7 @@ void SetTileRoomNum(int16_t sGridNo, uint8_t ubRoomNum) {
   gubWorldRoomInfo[sGridNo] = ubRoomNum;
 }
 
-void SetTileRangeRoomNum(SGPRect *pSelectRegion, uint8_t ubRoomNum) {
+void SetTileRangeRoomNum(struct GRect *pSelectRegion, uint8_t ubRoomNum) {
   int32_t cnt1, cnt2;
 
   for (cnt1 = pSelectRegion->iTop; cnt1 <= pSelectRegion->iBottom; cnt1++) {
@@ -259,7 +259,7 @@ void RemoveRoomRoof(uint16_t sGridNo, uint8_t bRoomNum, struct SOLDIERTYPE *pSol
   CalculateWorldWireFrameTiles(FALSE);
 }
 
-BOOLEAN AddSpecialTileRange(SGPRect *pSelectRegion) {
+BOOLEAN AddSpecialTileRange(struct GRect *pSelectRegion) {
   int32_t cnt1, cnt2;
 
   for (cnt1 = pSelectRegion->iTop; cnt1 <= pSelectRegion->iBottom; cnt1++) {
@@ -271,7 +271,7 @@ BOOLEAN AddSpecialTileRange(SGPRect *pSelectRegion) {
   return (TRUE);
 }
 
-BOOLEAN RemoveSpecialTileRange(SGPRect *pSelectRegion) {
+BOOLEAN RemoveSpecialTileRange(struct GRect *pSelectRegion) {
   int32_t cnt1, cnt2;
 
   for (cnt1 = pSelectRegion->iTop; cnt1 <= pSelectRegion->iBottom; cnt1++) {