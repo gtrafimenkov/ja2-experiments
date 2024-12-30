gtrafimenkov/ja2-rust

Implement FileMan operations in rust (#12)
* Implement FileMan operations in rust

* Bugfixes

* Cleaner interface with FileMan_OpenForWriting

* Cleaner interface with FileMan_OpenForAppending

* Cleaning up FileMan_Open interface

* Create cleaner interface with FileMan_OpenForReading

* Create cleaner interface withh FileMan_OpenForReading

* Create cleaner interface withh FileMan_OpenForReading

* Replace open for rw code

* Mark functions that need reimplementation

* Remove FileMan_ExistsNoDB

* Replace FileMan_Size

* Remove debug code that uses FileMan_GetPos

* Remove FileMan_GetPos

* Remove FileMan_CheckEndOfFile

* Solve parallel read problem

* Remove some autosave functionality and FileMan_GetFileWriteTime

* Replace FileMan functions in ja2lib

* A few of FileMan functions needed only in platform-win32 code

Sound and Video streaming

* Support video playback using new file functions

* Support sound playback using new file functions

* Remove debug logging

* Remove old FileMan functions

* Remove no longer needed code

* Fix compilation on Linux

unittester/rustlib-export.cpp
@@ -4,6 +4,8 @@
 extern "C" {
 #endif
 
+#include "rust_debug.h"
+#include "rust_fileman.h"
 #include "rust_platform.h"
 #include "rust_sam_sites.h"
 #include "rust_sector.h"
@@ -56,4 +58,182 @@ TEST(RustExport, GetStr) {
 TEST(RustExport, TownSectors) {
   EXPECT_EQ(BLANK_SECTOR, GetTownIdForSector(1, 1));
   EXPECT_EQ(OMERTA, GetTownIdForSector(9, 1));
+}
+
+TEST(RustExport, FileMan) {
+  File_RegisterSlfLibraries("tools/editor");
+
+  {
+    auto id = File_OpenForReading("Editor/EXITGRIDBUT.STI");
+    EXPECT_NE(id, FILE_ID_ERR);
+    EXPECT_TRUE(File_Close(id));
+  }
+
+  {
+    auto id = File_OpenForReading("Editor/EXITGRIDBUT.STI");
+    EXPECT_NE(id, FILE_ID_ERR);
+    uint8_t buf[10];
+    uint32_t read_bytes;
+    auto res = File_Read(id, buf, sizeof(buf), &read_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(10, read_bytes);
+    EXPECT_EQ('S', buf[0]);
+    EXPECT_EQ('T', buf[1]);
+    EXPECT_EQ('C', buf[2]);
+  }
+
+  {
+    auto id = File_OpenForReading("JA2.sln");
+    EXPECT_NE(id, FILE_ID_ERR);
+    uint8_t buf[4];
+    uint32_t read_bytes;
+    auto res = File_Read(id, buf, sizeof(buf), &read_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(sizeof(buf), read_bytes);
+    EXPECT_EQ('M', buf[0]);
+    EXPECT_EQ('i', buf[1]);
+    EXPECT_EQ('c', buf[2]);
+    EXPECT_EQ('r', buf[3]);
+
+    // seek backward from current position is not supported (because distance is u32)
+
+    // seek forward
+    {
+      EXPECT_TRUE(File_Read(id, buf, 1, &read_bytes));
+      EXPECT_EQ(1, read_bytes);
+      EXPECT_EQ('o', buf[0]);
+
+      // skip "sof"
+      EXPECT_TRUE(File_Seek(id, 3, FILE_SEEK_CURRENT));
+      EXPECT_TRUE(File_Read(id, buf, 1, &read_bytes));
+      EXPECT_EQ(1, read_bytes);
+      EXPECT_EQ('t', buf[0]);
+    }
+
+    EXPECT_TRUE(File_Close(id));
+  }
+}
+
+TEST(RustExport, FileSize) {
+  EXPECT_EQ(3444529, File_Size("tools/editor/Editor.slf"));
+
+  File_RegisterSlfLibraries("tools/editor");
+  EXPECT_EQ(5712, File_Size("Editor\\EXITGRIDBUT.STI"));
+}
+
+TEST(RustExport, FileManRW) {
+  // first write
+  {
+    auto id = File_OpenForWriting("./tmp-test-write01.txt");
+    EXPECT_NE(id, FILE_ID_ERR);
+    const char *message = "Hello there 1\n";
+    uint32_t written_bytes;
+    auto res = File_Write(id, message, strlen(message), &written_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(strlen(message), written_bytes);
+    EXPECT_TRUE(File_Close(id));
+  }
+
+  // second write
+  {
+    auto id = File_OpenForWriting("./tmp-test-write01.txt");
+    EXPECT_NE(id, FILE_ID_ERR);
+    const char *message = "Hello there 2\n";
+    uint32_t written_bytes;
+    auto res = File_Write(id, message, strlen(message), &written_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(strlen(message), written_bytes);
+    EXPECT_TRUE(File_Close(id));
+  }
+
+  // append
+  {
+    auto id = File_OpenForAppending("./tmp-test-write01.txt");
+    EXPECT_NE(id, FILE_ID_ERR);
+    const char *message = "Hello there 3\n";
+    uint32_t written_bytes;
+    auto res = File_Write(id, message, strlen(message), &written_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(strlen(message), written_bytes);
+    EXPECT_TRUE(File_Close(id));
+  }
+
+  // read
+  {
+    char buf[100];
+    auto id = File_OpenForReading("./tmp-test-write01.txt");
+    EXPECT_NE(id, FILE_ID_ERR);
+    uint32_t read_bytes;
+    auto res = File_Read(id, buf, sizeof(buf) - 1, &read_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(28, read_bytes);
+    EXPECT_TRUE(File_Close(id));
+
+    buf[read_bytes] = 0;
+    EXPECT_TRUE(strcmp(buf, "Hello there 2\nHello there 3\n") == 0);
+  }
+
+  Plat_DeleteFile("./tmp-test-write01.txt");
+}
+
+TEST(RustExport, ReadingGameRes) {
+  if (Plat_DirectoryExists("./ReleaseWithDebug/Data")) {
+    EXPECT_TRUE(File_RegisterSlfLibraries("./ReleaseWithDebug/Data"));
+    EXPECT_TRUE(File_Exists("FONTS\\LARGEFONT1.sti"));
+    EXPECT_TRUE(File_Exists("Anims\\VEHICLES\\JEEP.STI"));
+    EXPECT_TRUE(File_Exists("GENBUTN.STI"));
+    EXPECT_TRUE(File_Exists("TILESETS\\9\\trapdoor.sti"));
+  }
+}
+
+// TEST(RustExport, DebugLog) {
+//   EXPECT_TRUE(DebugLogWrite("hello there"));
+//   EXPECT_TRUE(DebugLogWrite("hello there again"));
+// }
+
+#include "TileEngine/StructureInternals.h"
+
+TEST(FileMan, LoadStructureData) {
+  if (Plat_DirectoryExists("./ReleaseWithDebug/Data")) {
+    EXPECT_TRUE(File_RegisterSlfLibraries("./ReleaseWithDebug/Data"));
+
+    const char *file_name = "ANIMS\\STRUCTDATA\\M_CROUCH.JSD";
+
+    FileID hInput = FILE_ID_ERR;
+    hInput = File_OpenForReading(file_name);
+    EXPECT_TRUE(hInput != 0);
+
+    STRUCTURE_FILE_HEADER Header;
+    UINT32 uiBytesRead;
+    BOOLEAN fOk = File_Read(hInput, &Header, sizeof(STRUCTURE_FILE_HEADER), &uiBytesRead);
+    EXPECT_TRUE(fOk);
+    EXPECT_EQ(16, uiBytesRead);
+  }
+}
+
+TEST(RustExport, FileManParallelRead) {
+  // It should be possible to open file two times and read content independently.
+  if (Plat_DirectoryExists("./ReleaseWithDebug/Data")) {
+    EXPECT_TRUE(File_RegisterSlfLibraries("./ReleaseWithDebug/Data"));
+
+    const char *file_name = "ANIMS\\STRUCTDATA\\M_CROUCH.JSD";
+
+    {
+      uint8_t buf[400];
+      FileID file_id = File_OpenForReading(file_name);
+      EXPECT_TRUE(file_id != 0);
+      uint32_t bytesRead = 0xffffffff;
+      EXPECT_TRUE(File_Read(file_id, &buf, sizeof(buf), &bytesRead));
+      EXPECT_EQ(400, bytesRead);
+    }
+
+    {
+      uint8_t buf[400];
+      FileID file_id = File_OpenForReading(file_name);
+      EXPECT_TRUE(file_id != 0);
+      uint32_t bytesRead = 0xffffffff;
+      EXPECT_TRUE(File_Read(file_id, &buf, sizeof(buf), &bytesRead));
+      EXPECT_EQ(400, bytesRead);
+    }
+  }
 }

gtrafimenkov/ja2-experiments

unittester/rustlib-export.cpp
@@ -4,6 +4,8 @@
 extern "C" {
 #endif
 
+#include "rust_debug.h"
+#include "rust_fileman.h"
 #include "rust_platform.h"
 #include "rust_sam_sites.h"
 #include "rust_sector.h"
@@ -56,4 +58,177 @@ TEST(RustExport, GetStr) {
 TEST(RustExport, TownSectors) {
   EXPECT_EQ(BLANK_SECTOR, GetTownIdForSector(1, 1));
   EXPECT_EQ(OMERTA, GetTownIdForSector(9, 1));
+}
+
+TEST(RustExport, FileMan) {
+  File_RegisterSlfLibraries("tools/editor");
+
+  {
+    auto id = File_OpenForReading("Editor/EXITGRIDBUT.STI");
+    EXPECT_NE(id, FILE_ID_ERR);
+    EXPECT_TRUE(File_Close(id));
+  }
+
+  {
+    auto id = File_OpenForReading("Editor/EXITGRIDBUT.STI");
+    EXPECT_NE(id, FILE_ID_ERR);
+    uint8_t buf[10];
+    uint32_t read_bytes;
+    auto res = File_Read(id, buf, sizeof(buf), &read_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(10, read_bytes);
+    EXPECT_EQ('S', buf[0]);
+    EXPECT_EQ('T', buf[1]);
+    EXPECT_EQ('C', buf[2]);
+  }
+
+  {
+    auto id = File_OpenForReading("SFI-SCLA.txt");
+    EXPECT_NE(id, FILE_ID_ERR);
+    uint8_t buf[4];
+    uint32_t read_bytes;
+    auto res = File_Read(id, buf, sizeof(buf), &read_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(sizeof(buf), read_bytes);
+    EXPECT_EQ('T', buf[0]);
+    EXPECT_EQ('h', buf[1]);
+    EXPECT_EQ('i', buf[2]);
+    EXPECT_EQ('s', buf[3]);
+
+    EXPECT_TRUE(File_Read(id, buf, 1, &read_bytes));
+    EXPECT_EQ(1, read_bytes);
+    EXPECT_EQ(' ', buf[0]);
+
+    // seek forward
+    EXPECT_TRUE(File_Seek(id, 3, FILE_SEEK_CURRENT));
+    EXPECT_TRUE(File_Read(id, buf, 1, &read_bytes));
+    EXPECT_EQ(1, read_bytes);
+    EXPECT_EQ('a', buf[0]);
+
+    EXPECT_TRUE(File_Close(id));
+  }
+}
+
+TEST(RustExport, FileSize) {
+  EXPECT_EQ(3444529, File_Size("tools/editor/Editor.slf"));
+
+  File_RegisterSlfLibraries("tools/editor");
+  EXPECT_EQ(5712, File_Size("Editor\\EXITGRIDBUT.STI"));
+}
+
+TEST(RustExport, FileManRW) {
+  // first write
+  {
+    auto id = File_OpenForWriting("./tmp-test-write01.txt");
+    EXPECT_NE(id, FILE_ID_ERR);
+    const char *message = "Hello there 1\n";
+    uint32_t written_bytes;
+    auto res = File_Write(id, message, strlen(message), &written_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(strlen(message), written_bytes);
+    EXPECT_TRUE(File_Close(id));
+  }
+
+  // second write
+  {
+    auto id = File_OpenForWriting("./tmp-test-write01.txt");
+    EXPECT_NE(id, FILE_ID_ERR);
+    const char *message = "Hello there 2\n";
+    uint32_t written_bytes;
+    auto res = File_Write(id, message, strlen(message), &written_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(strlen(message), written_bytes);
+    EXPECT_TRUE(File_Close(id));
+  }
+
+  // append
+  {
+    auto id = File_OpenForAppending("./tmp-test-write01.txt");
+    EXPECT_NE(id, FILE_ID_ERR);
+    const char *message = "Hello there 3\n";
+    uint32_t written_bytes;
+    auto res = File_Write(id, message, strlen(message), &written_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(strlen(message), written_bytes);
+    EXPECT_TRUE(File_Close(id));
+  }
+
+  // read
+  {
+    char buf[100];
+    auto id = File_OpenForReading("./tmp-test-write01.txt");
+    EXPECT_NE(id, FILE_ID_ERR);
+    uint32_t read_bytes;
+    auto res = File_Read(id, buf, sizeof(buf) - 1, &read_bytes);
+    EXPECT_TRUE(res);
+    EXPECT_EQ(28, read_bytes);
+    EXPECT_TRUE(File_Close(id));
+
+    buf[read_bytes] = 0;
+    EXPECT_TRUE(strcmp(buf, "Hello there 2\nHello there 3\n") == 0);
+  }
+
+  Plat_DeleteFile("./tmp-test-write01.txt");
+}
+
+TEST(RustExport, ReadingGameRes) {
+  if (Plat_DirectoryExists("./ReleaseWithDebug/Data")) {
+    EXPECT_TRUE(File_RegisterSlfLibraries("./ReleaseWithDebug/Data"));
+    EXPECT_TRUE(File_Exists("FONTS\\LARGEFONT1.sti"));
+    EXPECT_TRUE(File_Exists("Anims\\VEHICLES\\JEEP.STI"));
+    EXPECT_TRUE(File_Exists("GENBUTN.STI"));
+    EXPECT_TRUE(File_Exists("TILESETS\\9\\trapdoor.sti"));
+  }
+}
+
+// TEST(RustExport, DebugLog) {
+//   EXPECT_TRUE(DebugLogWrite("hello there"));
+//   EXPECT_TRUE(DebugLogWrite("hello there again"));
+// }
+
+#include "TileEngine/StructureInternals.h"
+
+TEST(FileMan, LoadStructureData) {
+  if (Plat_DirectoryExists("./ReleaseWithDebug/Data")) {
+    EXPECT_TRUE(File_RegisterSlfLibraries("./ReleaseWithDebug/Data"));
+
+    const char *file_name = "ANIMS\\STRUCTDATA\\M_CROUCH.JSD";
+
+    FileID hInput = FILE_ID_ERR;
+    hInput = File_OpenForReading(file_name);
+    EXPECT_TRUE(hInput != 0);
+
+    STRUCTURE_FILE_HEADER Header;
+    uint32_t uiBytesRead;
+    BOOLEAN fOk = File_Read(hInput, &Header, sizeof(STRUCTURE_FILE_HEADER), &uiBytesRead);
+    EXPECT_TRUE(fOk);
+    EXPECT_EQ(16, uiBytesRead);
+  }
+}
+
+TEST(RustExport, FileManParallelRead) {
+  // It should be possible to open file two times and read content independently.
+  if (Plat_DirectoryExists("./ReleaseWithDebug/Data")) {
+    EXPECT_TRUE(File_RegisterSlfLibraries("./ReleaseWithDebug/Data"));
+
+    const char *file_name = "ANIMS\\STRUCTDATA\\M_CROUCH.JSD";
+
+    {
+      uint8_t buf[400];
+      FileID file_id = File_OpenForReading(file_name);
+      EXPECT_TRUE(file_id != 0);
+      uint32_t bytesRead = 0xffffffff;
+      EXPECT_TRUE(File_Read(file_id, &buf, sizeof(buf), &bytesRead));
+      EXPECT_EQ(400, bytesRead);
+    }
+
+    {
+      uint8_t buf[400];
+      FileID file_id = File_OpenForReading(file_name);
+      EXPECT_TRUE(file_id != 0);
+      uint32_t bytesRead = 0xffffffff;
+      EXPECT_TRUE(File_Read(file_id, &buf, sizeof(buf), &bytesRead));
+      EXPECT_EQ(400, bytesRead);
+    }
+  }
 }