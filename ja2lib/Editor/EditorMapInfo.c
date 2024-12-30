gtrafimenkov/ja2-rust

Rename for readability (#30)
* State 2023-07-02 15:54:29

* State 2023-07-02 15:57:09

* State 2023-07-02 15:58:38

* State 2023-07-02 16:02:18

* State 2023-07-02 16:12:29

* State 2023-07-02 16:46:52

ja2lib/Editor/EditorMapInfo.c
@@ -60,11 +60,11 @@ void SetupTextInputForMapInfo() {
   AddUserInputField(NULL);  // just so we can use short cut keys while not typing.
 
   // light rgb fields
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peRed);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.red);
   AddTextInputField(10, 394, 25, 18, MSYS_PRIORITY_NORMAL, str, 3, INPUTTYPE_NUMERICSTRICT);
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peGreen);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.green);
   AddTextInputField(10, 414, 25, 18, MSYS_PRIORITY_NORMAL, str, 3, INPUTTYPE_NUMERICSTRICT);
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peBlue);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.blue);
   AddTextInputField(10, 434, 25, 18, MSYS_PRIORITY_NORMAL, str, 3, INPUTTYPE_NUMERICSTRICT);
 
   swprintf(str, ARR_SIZE(str), L"%d", gsLightRadius);
@@ -134,11 +134,11 @@ void UpdateMapInfoFields() {
   CHAR16 str[10];
   // Update the text fields to reflect the validated values.
   // light rgb fields
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peRed);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.red);
   SetInputFieldStringWith16BitString(1, str);
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peGreen);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.green);
   SetInputFieldStringWith16BitString(2, str);
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peBlue);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.blue);
   SetInputFieldStringWith16BitString(3, str);
 
   swprintf(str, ARR_SIZE(str), L"%d", gsLightRadius);
@@ -161,19 +161,19 @@ void ExtractAndUpdateMapInfo() {
   BOOLEAN fUpdateLight1 = FALSE;
   // extract light1 colors
   temp = min(GetNumericStrictValueFromField(1), 255);
-  if (temp != -1 && temp != gEditorLightColor.peRed) {
+  if (temp != -1 && temp != gEditorLightColor.red) {
     fUpdateLight1 = TRUE;
-    gEditorLightColor.peRed = (UINT8)temp;
+    gEditorLightColor.red = (UINT8)temp;
   }
   temp = min(GetNumericStrictValueFromField(2), 255);
-  if (temp != -1 && temp != gEditorLightColor.peGreen) {
+  if (temp != -1 && temp != gEditorLightColor.green) {
     fUpdateLight1 = TRUE;
-    gEditorLightColor.peGreen = (UINT8)temp;
+    gEditorLightColor.green = (UINT8)temp;
   }
   temp = min(GetNumericStrictValueFromField(3), 255);
-  if (temp != -1 && temp != gEditorLightColor.peBlue) {
+  if (temp != -1 && temp != gEditorLightColor.blue) {
     fUpdateLight1 = TRUE;
-    gEditorLightColor.peBlue = (UINT8)temp;
+    gEditorLightColor.blue = (UINT8)temp;
   }
   if (fUpdateLight1) {
     gfEditorForceShadeTableRebuild = TRUE;

ja2lib/JAScreens.c
@@ -631,7 +631,7 @@ UINT32 SexScreenHandle(void) {
   static UINT32 guiSMILY;
   static INT8 bCurFrame = 0;
   static UINT32 uiTimeOfLastUpdate = 0, uiTime;
-  struct ETRLEObject *pTrav;
+  struct Subimage *pTrav;
   struct VObject *hVObject;
   INT16 sX, sY;
 
@@ -695,10 +695,10 @@ UINT32 SexScreenHandle(void) {
 
   // Calculate smily face positions...
   GetVideoObject(&hVObject, guiSMILY);
-  pTrav = &(hVObject->pETRLEObject[0]);
+  pTrav = &(hVObject->subimages[0]);
 
-  sX = (INT16)((640 - pTrav->usWidth) / 2);
-  sY = (INT16)((480 - pTrav->usHeight) / 2);
+  sX = (INT16)((640 - pTrav->width) / 2);
+  sY = (INT16)((480 - pTrav->height) / 2);
 
   if (bCurFrame < 24) {
     BltVObjectFromIndex(vsFB, guiSMILY, 0, sX, sY);
@@ -706,7 +706,7 @@ UINT32 SexScreenHandle(void) {
     BltVObjectFromIndex(vsFB, guiSMILY, (INT8)(bCurFrame % 8), sX, sY);
   }
 
-  InvalidateRegion(sX, sY, (INT16)(sX + pTrav->usWidth), (INT16)(sY + pTrav->usHeight));
+  InvalidateRegion(sX, sY, (INT16)(sX + pTrav->width), (INT16)(sY + pTrav->height));
 
   return (SEX_SCREEN);
 }

ja2lib/Laptop/BobbyRGuns.c
@@ -790,7 +790,7 @@ BOOLEAN DisplayAmmoInfo(UINT16 usIndex, UINT16 usTextPosY, BOOLEAN fUsed, UINT16
 BOOLEAN DisplayBigItemImage(UINT16 usIndex, UINT16 PosY) {
   INT16 PosX, sCenX, sCenY;
   UINT32 usWidth;
-  struct ETRLEObject *pTrav;
+  struct Subimage *pTrav;
   INVTYPE *pItem;
   UINT32 uiImage;
   struct VObject *hPixHandle;
@@ -801,13 +801,13 @@ BOOLEAN DisplayBigItemImage(UINT16 usIndex, UINT16 PosY) {
   LoadTileGraphicForItem(pItem, &uiImage);
 
   GetVideoObject(&hPixHandle, uiImage);
-  pTrav = &(hPixHandle->pETRLEObject[0]);
+  pTrav = &(hPixHandle->subimages[0]);
 
   // center picture in frame
-  usWidth = (UINT32)pTrav->usWidth;
+  usWidth = (UINT32)pTrav->width;
   //	sCenX = PosX + ( abs( BOBBYR_GRID_PIC_WIDTH - usWidth ) / 2 );
   //	sCenY = PosY + 8;
-  sCenX = PosX + (abs((INT32)((INT32)BOBBYR_GRID_PIC_WIDTH - usWidth)) / 2) - pTrav->sOffsetX;
+  sCenX = PosX + (abs((INT32)((INT32)BOBBYR_GRID_PIC_WIDTH - usWidth)) / 2) - pTrav->x_offset;
   sCenY = PosY + 8;
 
   // blt the shadow of the item

ja2lib/SGP/VObject.h
@@ -117,9 +117,9 @@ BOOLEAN GetETRLEPixelValue(UINT8 *pDest, struct VObject *hVObject, UINT16 usETLR
 struct VObject *GetPrimaryVideoObject();
 struct VObject *GetBackBufferVideoObject();
 
-BOOLEAN GetVideoObjectETRLEProperties(struct VObject *hVObject, struct ETRLEObject *pETRLEObject,
+BOOLEAN GetVideoObjectETRLEProperties(struct VObject *hVObject, struct Subimage *subimages,
                                       UINT16 usIndex);
-BOOLEAN GetVideoObjectETRLEPropertiesFromIndex(UINT32 uiVideoObject, struct ETRLEObject *pETRLEObject,
+BOOLEAN GetVideoObjectETRLEPropertiesFromIndex(UINT32 uiVideoObject, struct Subimage *subimages,
                                                UINT16 usIndex);
 BOOLEAN GetVideoObjectETRLESubregionProperties(UINT32 uiVideoObject, UINT16 usIndex,
                                                UINT16 *pusWidth, UINT16 *pusHeight);

ja2lib/Tactical/RottingCorpses.c
@@ -1171,12 +1171,12 @@ UINT16 CreateCorpsePaletteTables(ROTTING_CORPSE *pCorpse) {
   // create the basic shade table
   for (uiCount = 0; uiCount < 256; uiCount++) {
     // combine the rgb of the light color with the object's palette
-    LightPal[uiCount].peRed = (UINT8)(min(
-        (UINT16)pCorpse->p8BPPPalette[uiCount].peRed + (UINT16)gpLightColors[0].peRed, 255));
-    LightPal[uiCount].peGreen = (UINT8)(min(
-        (UINT16)pCorpse->p8BPPPalette[uiCount].peGreen + (UINT16)gpLightColors[0].peGreen, 255));
-    LightPal[uiCount].peBlue = (UINT8)(min(
-        (UINT16)pCorpse->p8BPPPalette[uiCount].peBlue + (UINT16)gpLightColors[0].peBlue, 255));
+    LightPal[uiCount].red = (UINT8)(min(
+        (UINT16)pCorpse->p8BPPPalette[uiCount].red + (UINT16)gpLightColors[0].red, 255));
+    LightPal[uiCount].green = (UINT8)(min(
+        (UINT16)pCorpse->p8BPPPalette[uiCount].green + (UINT16)gpLightColors[0].green, 255));
+    LightPal[uiCount].blue = (UINT8)(min(
+        (UINT16)pCorpse->p8BPPPalette[uiCount].blue + (UINT16)gpLightColors[0].blue, 255));
   }
   // build the shade tables
   CreateCorpseShadedPalette(pCorpse, 0, LightPal);

ja2lib/Tactical/SoldierControl.c
@@ -4658,9 +4658,9 @@ BOOLEAN CreateSoldierPalettes(struct SOLDIERTYPE *pSoldier) {
   // Build a grayscale palette for testing grayout of mercs
   // for(uiCount=0; uiCount < 256; uiCount++)
   //{
-  //	Pal[uiCount].peRed=(UINT8)(uiCount%128)+128;
-  //	Pal[uiCount].peGreen=(UINT8)(uiCount%128)+128;
-  //	Pal[uiCount].peBlue=(UINT8)(uiCount%128)+128;
+  //	Pal[uiCount].red=(UINT8)(uiCount%128)+128;
+  //	Pal[uiCount].green=(UINT8)(uiCount%128)+128;
+  //	Pal[uiCount].blue=(UINT8)(uiCount%128)+128;
   //}
   pSoldier->pEffectShades[0] =
       Create16BPPPaletteShaded(pSoldier->p8BPPPalette, 100, 100, 100, TRUE);
@@ -4982,9 +4982,9 @@ BOOLEAN SetPaletteReplacement(struct SGPPaletteEntry *p8BPPPalette, PaletteRepID
 
   for (cnt2 = gpPaletteSubRanges[ubType].ubStart; cnt2 <= gpPaletteSubRanges[ubType].ubEnd;
        cnt2++) {
-    p8BPPPalette[cnt2].peRed = gpPalRep[ubPalIndex].r[cnt2 - gpPaletteSubRanges[ubType].ubStart];
-    p8BPPPalette[cnt2].peGreen = gpPalRep[ubPalIndex].g[cnt2 - gpPaletteSubRanges[ubType].ubStart];
-    p8BPPPalette[cnt2].peBlue = gpPalRep[ubPalIndex].b[cnt2 - gpPaletteSubRanges[ubType].ubStart];
+    p8BPPPalette[cnt2].red = gpPalRep[ubPalIndex].r[cnt2 - gpPaletteSubRanges[ubType].ubStart];
+    p8BPPPalette[cnt2].green = gpPalRep[ubPalIndex].g[cnt2 - gpPaletteSubRanges[ubType].ubStart];
+    p8BPPPalette[cnt2].blue = gpPalRep[ubPalIndex].b[cnt2 - gpPaletteSubRanges[ubType].ubStart];
   }
 
   return (TRUE);
@@ -7704,13 +7704,13 @@ UINT16 *CreateEnemyGlow16BPPPalette(struct SGPPaletteEntry *pPalette, UINT32 rsc
   p16BPPPalette = (UINT16 *)MemAlloc(sizeof(UINT16) * 256);
 
   for (cnt = 0; cnt < 256; cnt++) {
-    gmod = (pPalette[cnt].peGreen);
-    bmod = (pPalette[cnt].peBlue);
+    gmod = (pPalette[cnt].green);
+    bmod = (pPalette[cnt].blue);
 
-    rmod = max(rscale, (pPalette[cnt].peRed));
+    rmod = max(rscale, (pPalette[cnt].red));
 
     if (fAdjustGreen) {
-      gmod = max(gscale, (pPalette[cnt].peGreen));
+      gmod = max(gscale, (pPalette[cnt].green));
     }
 
     r = (UINT8)min(rmod, 255);
@@ -7743,8 +7743,8 @@ UINT16 *CreateEnemyGreyGlow16BPPPalette(struct SGPPaletteEntry *pPalette, UINT32
   p16BPPPalette = (UINT16 *)MemAlloc(sizeof(UINT16) * 256);
 
   for (cnt = 0; cnt < 256; cnt++) {
-    lumin = (pPalette[cnt].peRed * 299 / 1000) + (pPalette[cnt].peGreen * 587 / 1000) +
-            (pPalette[cnt].peBlue * 114 / 1000);
+    lumin = (pPalette[cnt].red * 299 / 1000) + (pPalette[cnt].green * 587 / 1000) +
+            (pPalette[cnt].blue * 114 / 1000);
     rmod = (100 * lumin) / 256;
     gmod = (100 * lumin) / 256;
     bmod = (100 * lumin) / 256;
@@ -7978,7 +7978,7 @@ void SelectMoveAnimationFromStance(struct SOLDIERTYPE *pSoldier) {
 
 void GetActualSoldierAnimDims(struct SOLDIERTYPE *pSoldier, INT16 *psHeight, INT16 *psWidth) {
   UINT16 usAnimSurface;
-  struct ETRLEObject *pTrav;
+  struct Subimage *pTrav;
 
   usAnimSurface = GetSoldierAnimationSurface(pSoldier, pSoldier->usAnimState);
 
@@ -8002,15 +8002,15 @@ void GetActualSoldierAnimDims(struct SOLDIERTYPE *pSoldier, INT16 *psHeight, INT
   if (pSoldier->usAniFrame >= gAnimSurfaceDatabase[usAnimSurface].hVideoObject->usNumberOfObjects) {
   }
 
-  pTrav = &(gAnimSurfaceDatabase[usAnimSurface].hVideoObject->pETRLEObject[pSoldier->usAniFrame]);
+  pTrav = &(gAnimSurfaceDatabase[usAnimSurface].hVideoObject->subimages[pSoldier->usAniFrame]);
 
-  *psHeight = (INT16)pTrav->usHeight;
-  *psWidth = (INT16)pTrav->usWidth;
+  *psHeight = (INT16)pTrav->height;
+  *psWidth = (INT16)pTrav->width;
 }
 
 void GetActualSoldierAnimOffsets(struct SOLDIERTYPE *pSoldier, INT16 *sOffsetX, INT16 *sOffsetY) {
   UINT16 usAnimSurface;
-  struct ETRLEObject *pTrav;
+  struct Subimage *pTrav;
 
   usAnimSurface = GetSoldierAnimationSurface(pSoldier, pSoldier->usAnimState);
 
@@ -8027,10 +8027,10 @@ void GetActualSoldierAnimOffsets(struct SOLDIERTYPE *pSoldier, INT16 *sOffsetX, 
     return;
   }
 
-  pTrav = &(gAnimSurfaceDatabase[usAnimSurface].hVideoObject->pETRLEObject[pSoldier->usAniFrame]);
+  pTrav = &(gAnimSurfaceDatabase[usAnimSurface].hVideoObject->subimages[pSoldier->usAniFrame]);
 
-  *sOffsetX = (INT16)pTrav->sOffsetX;
-  *sOffsetY = (INT16)pTrav->sOffsetY;
+  *sOffsetX = (INT16)pTrav->x_offset;
+  *sOffsetY = (INT16)pTrav->y_offset;
 }
 
 void SetSoldierLocatorOffsets(struct SOLDIERTYPE *pSoldier) {

gtrafimenkov/ja2-experiments

ja2lib/Editor/EditorMapInfo.c
@@ -64,11 +64,11 @@ void SetupTextInputForMapInfo() {
   AddUserInputField(NULL);  // just so we can use short cut keys while not typing.
 
   // light rgb fields
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peRed);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.red);
   AddTextInputField(10, 394, 25, 18, MSYS_PRIORITY_NORMAL, str, 3, INPUTTYPE_NUMERICSTRICT);
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peGreen);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.green);
   AddTextInputField(10, 414, 25, 18, MSYS_PRIORITY_NORMAL, str, 3, INPUTTYPE_NUMERICSTRICT);
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peBlue);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.blue);
   AddTextInputField(10, 434, 25, 18, MSYS_PRIORITY_NORMAL, str, 3, INPUTTYPE_NUMERICSTRICT);
 
   swprintf(str, ARR_SIZE(str), L"%d", gsLightRadius);
@@ -138,11 +138,11 @@ void UpdateMapInfoFields() {
   wchar_t str[10];
   // Update the text fields to reflect the validated values.
   // light rgb fields
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peRed);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.red);
   SetInputFieldStringWith16BitString(1, str);
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peGreen);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.green);
   SetInputFieldStringWith16BitString(2, str);
-  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.peBlue);
+  swprintf(str, ARR_SIZE(str), L"%d", gEditorLightColor.blue);
   SetInputFieldStringWith16BitString(3, str);
 
   swprintf(str, ARR_SIZE(str), L"%d", gsLightRadius);
@@ -165,19 +165,19 @@ void ExtractAndUpdateMapInfo() {
   BOOLEAN fUpdateLight1 = FALSE;
   // extract light1 colors
   temp = min(GetNumericStrictValueFromField(1), 255);
-  if (temp != -1 && temp != gEditorLightColor.peRed) {
+  if (temp != -1 && temp != gEditorLightColor.red) {
     fUpdateLight1 = TRUE;
-    gEditorLightColor.peRed = (uint8_t)temp;
+    gEditorLightColor.red = (uint8_t)temp;
   }
   temp = min(GetNumericStrictValueFromField(2), 255);
-  if (temp != -1 && temp != gEditorLightColor.peGreen) {
+  if (temp != -1 && temp != gEditorLightColor.green) {
     fUpdateLight1 = TRUE;
-    gEditorLightColor.peGreen = (uint8_t)temp;
+    gEditorLightColor.green = (uint8_t)temp;
   }
   temp = min(GetNumericStrictValueFromField(3), 255);
-  if (temp != -1 && temp != gEditorLightColor.peBlue) {
+  if (temp != -1 && temp != gEditorLightColor.blue) {
     fUpdateLight1 = TRUE;
-    gEditorLightColor.peBlue = (uint8_t)temp;
+    gEditorLightColor.blue = (uint8_t)temp;
   }
   if (fUpdateLight1) {
     gfEditorForceShadeTableRebuild = TRUE;

ja2lib/JAScreens.c
@@ -635,7 +635,7 @@ uint32_t SexScreenHandle(void) {
   static uint32_t guiSMILY;
   static int8_t bCurFrame = 0;
   static uint32_t uiTimeOfLastUpdate = 0, uiTime;
-  struct ETRLEObject *pTrav;
+  struct Subimage *pTrav;
   struct VObject *hVObject;
   int16_t sX, sY;
 
@@ -699,10 +699,10 @@ uint32_t SexScreenHandle(void) {
 
   // Calculate smily face positions...
   GetVideoObject(&hVObject, guiSMILY);
-  pTrav = &(hVObject->pETRLEObject[0]);
+  pTrav = &(hVObject->subimages[0]);
 
-  sX = (int16_t)((640 - pTrav->usWidth) / 2);
-  sY = (int16_t)((480 - pTrav->usHeight) / 2);
+  sX = (int16_t)((640 - pTrav->width) / 2);
+  sY = (int16_t)((480 - pTrav->height) / 2);
 
   if (bCurFrame < 24) {
     BltVObjectFromIndex(vsFB, guiSMILY, 0, sX, sY);
@@ -710,7 +710,7 @@ uint32_t SexScreenHandle(void) {
     BltVObjectFromIndex(vsFB, guiSMILY, (int8_t)(bCurFrame % 8), sX, sY);
   }
 
-  InvalidateRegion(sX, sY, (int16_t)(sX + pTrav->usWidth), (int16_t)(sY + pTrav->usHeight));
+  InvalidateRegion(sX, sY, (int16_t)(sX + pTrav->width), (int16_t)(sY + pTrav->height));
 
   return (SEX_SCREEN);
 }

ja2lib/Laptop/BobbyRGuns.c
@@ -799,7 +799,7 @@ BOOLEAN DisplayAmmoInfo(uint16_t usIndex, uint16_t usTextPosY, BOOLEAN fUsed,
 BOOLEAN DisplayBigItemImage(uint16_t usIndex, uint16_t PosY) {
   int16_t PosX, sCenX, sCenY;
   uint32_t usWidth;
-  struct ETRLEObject *pTrav;
+  struct Subimage *pTrav;
   INVTYPE *pItem;
   uint32_t uiImage;
   struct VObject *hPixHandle;
@@ -810,13 +810,13 @@ BOOLEAN DisplayBigItemImage(uint16_t usIndex, uint16_t PosY) {
   LoadTileGraphicForItem(pItem, &uiImage);
 
   GetVideoObject(&hPixHandle, uiImage);
-  pTrav = &(hPixHandle->pETRLEObject[0]);
+  pTrav = &(hPixHandle->subimages[0]);
 
   // center picture in frame
-  usWidth = (uint32_t)pTrav->usWidth;
+  usWidth = (uint32_t)pTrav->width;
   //	sCenX = PosX + ( abs( BOBBYR_GRID_PIC_WIDTH - usWidth ) / 2 );
   //	sCenY = PosY + 8;
-  sCenX = PosX + (abs((int32_t)((int32_t)BOBBYR_GRID_PIC_WIDTH - usWidth)) / 2) - pTrav->sOffsetX;
+  sCenX = PosX + (abs((int32_t)((int32_t)BOBBYR_GRID_PIC_WIDTH - usWidth)) / 2) - pTrav->x_offset;
   sCenY = PosY + 8;
 
   // blt the shadow of the item

ja2lib/SGP/VObject.h
@@ -121,10 +121,10 @@ BOOLEAN GetETRLEPixelValue(uint8_t *pDest, struct VObject *hVObject, uint16_t us
 struct VObject *GetPrimaryVideoObject();
 struct VObject *GetBackBufferVideoObject();
 
-BOOLEAN GetVideoObjectETRLEProperties(struct VObject *hVObject, struct ETRLEObject *pETRLEObject,
+BOOLEAN GetVideoObjectETRLEProperties(struct VObject *hVObject, struct Subimage *subimages,
                                       uint16_t usIndex);
-BOOLEAN GetVideoObjectETRLEPropertiesFromIndex(uint32_t uiVideoObject,
-                                               struct ETRLEObject *pETRLEObject, uint16_t usIndex);
+BOOLEAN GetVideoObjectETRLEPropertiesFromIndex(uint32_t uiVideoObject, struct Subimage *subimages,
+                                               uint16_t usIndex);
 BOOLEAN GetVideoObjectETRLESubregionProperties(uint32_t uiVideoObject, uint16_t usIndex,
                                                uint16_t *pusWidth, uint16_t *pusHeight);
 

ja2lib/Tactical/RottingCorpses.c
@@ -1175,13 +1175,12 @@ uint16_t CreateCorpsePaletteTables(ROTTING_CORPSE *pCorpse) {
   // create the basic shade table
   for (uiCount = 0; uiCount < 256; uiCount++) {
     // combine the rgb of the light color with the object's palette
-    LightPal[uiCount].peRed = (uint8_t)(min(
-        (uint16_t)pCorpse->p8BPPPalette[uiCount].peRed + (uint16_t)gpLightColors[0].peRed, 255));
-    LightPal[uiCount].peGreen = (uint8_t)(min(
-        (uint16_t)pCorpse->p8BPPPalette[uiCount].peGreen + (uint16_t)gpLightColors[0].peGreen,
-        255));
-    LightPal[uiCount].peBlue = (uint8_t)(min(
-        (uint16_t)pCorpse->p8BPPPalette[uiCount].peBlue + (uint16_t)gpLightColors[0].peBlue, 255));
+    LightPal[uiCount].red = (uint8_t)(min(
+        (uint16_t)pCorpse->p8BPPPalette[uiCount].red + (uint16_t)gpLightColors[0].red, 255));
+    LightPal[uiCount].green = (uint8_t)(min(
+        (uint16_t)pCorpse->p8BPPPalette[uiCount].green + (uint16_t)gpLightColors[0].green, 255));
+    LightPal[uiCount].blue = (uint8_t)(min(
+        (uint16_t)pCorpse->p8BPPPalette[uiCount].blue + (uint16_t)gpLightColors[0].blue, 255));
   }
   // build the shade tables
   CreateCorpseShadedPalette(pCorpse, 0, LightPal);

ja2lib/Tactical/SoldierControl.c
@@ -4662,9 +4662,9 @@ BOOLEAN CreateSoldierPalettes(struct SOLDIERTYPE *pSoldier) {
   // Build a grayscale palette for testing grayout of mercs
   // for(uiCount=0; uiCount < 256; uiCount++)
   //{
-  //	Pal[uiCount].peRed=(uint8_t)(uiCount%128)+128;
-  //	Pal[uiCount].peGreen=(uint8_t)(uiCount%128)+128;
-  //	Pal[uiCount].peBlue=(uint8_t)(uiCount%128)+128;
+  //	Pal[uiCount].red=(uint8_t)(uiCount%128)+128;
+  //	Pal[uiCount].green=(uint8_t)(uiCount%128)+128;
+  //	Pal[uiCount].blue=(uint8_t)(uiCount%128)+128;
   //}
   pSoldier->pEffectShades[0] =
       Create16BPPPaletteShaded(pSoldier->p8BPPPalette, 100, 100, 100, TRUE);
@@ -4986,9 +4986,9 @@ BOOLEAN SetPaletteReplacement(struct SGPPaletteEntry *p8BPPPalette, PaletteRepID
 
   for (cnt2 = gpPaletteSubRanges[ubType].ubStart; cnt2 <= gpPaletteSubRanges[ubType].ubEnd;
        cnt2++) {
-    p8BPPPalette[cnt2].peRed = gpPalRep[ubPalIndex].r[cnt2 - gpPaletteSubRanges[ubType].ubStart];
-    p8BPPPalette[cnt2].peGreen = gpPalRep[ubPalIndex].g[cnt2 - gpPaletteSubRanges[ubType].ubStart];
-    p8BPPPalette[cnt2].peBlue = gpPalRep[ubPalIndex].b[cnt2 - gpPaletteSubRanges[ubType].ubStart];
+    p8BPPPalette[cnt2].red = gpPalRep[ubPalIndex].r[cnt2 - gpPaletteSubRanges[ubType].ubStart];
+    p8BPPPalette[cnt2].green = gpPalRep[ubPalIndex].g[cnt2 - gpPaletteSubRanges[ubType].ubStart];
+    p8BPPPalette[cnt2].blue = gpPalRep[ubPalIndex].b[cnt2 - gpPaletteSubRanges[ubType].ubStart];
   }
 
   return (TRUE);
@@ -7713,13 +7713,13 @@ uint16_t *CreateEnemyGlow16BPPPalette(struct SGPPaletteEntry *pPalette, uint32_t
   p16BPPPalette = (uint16_t *)MemAlloc(sizeof(uint16_t) * 256);
 
   for (cnt = 0; cnt < 256; cnt++) {
-    gmod = (pPalette[cnt].peGreen);
-    bmod = (pPalette[cnt].peBlue);
+    gmod = (pPalette[cnt].green);
+    bmod = (pPalette[cnt].blue);
 
-    rmod = max(rscale, (pPalette[cnt].peRed));
+    rmod = max(rscale, (pPalette[cnt].red));
 
     if (fAdjustGreen) {
-      gmod = max(gscale, (pPalette[cnt].peGreen));
+      gmod = max(gscale, (pPalette[cnt].green));
     }
 
     r = (uint8_t)min(rmod, 255);
@@ -7752,8 +7752,8 @@ uint16_t *CreateEnemyGreyGlow16BPPPalette(struct SGPPaletteEntry *pPalette, uint
   p16BPPPalette = (uint16_t *)MemAlloc(sizeof(uint16_t) * 256);
 
   for (cnt = 0; cnt < 256; cnt++) {
-    lumin = (pPalette[cnt].peRed * 299 / 1000) + (pPalette[cnt].peGreen * 587 / 1000) +
-            (pPalette[cnt].peBlue * 114 / 1000);
+    lumin = (pPalette[cnt].red * 299 / 1000) + (pPalette[cnt].green * 587 / 1000) +
+            (pPalette[cnt].blue * 114 / 1000);
     rmod = (100 * lumin) / 256;
     gmod = (100 * lumin) / 256;
     bmod = (100 * lumin) / 256;
@@ -7987,7 +7987,7 @@ void SelectMoveAnimationFromStance(struct SOLDIERTYPE *pSoldier) {
 
 void GetActualSoldierAnimDims(struct SOLDIERTYPE *pSoldier, int16_t *psHeight, int16_t *psWidth) {
   uint16_t usAnimSurface;
-  struct ETRLEObject *pTrav;
+  struct Subimage *pTrav;
 
   usAnimSurface = GetSoldierAnimationSurface(pSoldier, pSoldier->usAnimState);
 
@@ -8011,16 +8011,16 @@ void GetActualSoldierAnimDims(struct SOLDIERTYPE *pSoldier, int16_t *psHeight, i
   if (pSoldier->usAniFrame >= gAnimSurfaceDatabase[usAnimSurface].hVideoObject->usNumberOfObjects) {
   }
 
-  pTrav = &(gAnimSurfaceDatabase[usAnimSurface].hVideoObject->pETRLEObject[pSoldier->usAniFrame]);
+  pTrav = &(gAnimSurfaceDatabase[usAnimSurface].hVideoObject->subimages[pSoldier->usAniFrame]);
 
-  *psHeight = (int16_t)pTrav->usHeight;
-  *psWidth = (int16_t)pTrav->usWidth;
+  *psHeight = (int16_t)pTrav->height;
+  *psWidth = (int16_t)pTrav->width;
 }
 
 void GetActualSoldierAnimOffsets(struct SOLDIERTYPE *pSoldier, int16_t *sOffsetX,
                                  int16_t *sOffsetY) {
   uint16_t usAnimSurface;
-  struct ETRLEObject *pTrav;
+  struct Subimage *pTrav;
 
   usAnimSurface = GetSoldierAnimationSurface(pSoldier, pSoldier->usAnimState);
 
@@ -8037,10 +8037,10 @@ void GetActualSoldierAnimOffsets(struct SOLDIERTYPE *pSoldier, int16_t *sOffsetX
     return;
   }
 
-  pTrav = &(gAnimSurfaceDatabase[usAnimSurface].hVideoObject->pETRLEObject[pSoldier->usAniFrame]);
+  pTrav = &(gAnimSurfaceDatabase[usAnimSurface].hVideoObject->subimages[pSoldier->usAniFrame]);
 
-  *sOffsetX = (int16_t)pTrav->sOffsetX;
-  *sOffsetY = (int16_t)pTrav->sOffsetY;
+  *sOffsetX = (int16_t)pTrav->x_offset;
+  *sOffsetY = (int16_t)pTrav->y_offset;
 }
 
 void SetSoldierLocatorOffsets(struct SOLDIERTYPE *pSoldier) {