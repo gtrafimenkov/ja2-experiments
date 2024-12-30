gtrafimenkov/ja2-rust

Move some GameClock code to rust (part 3) (#24)
* State 2023-06-23 06:59:25

* State 2023-06-23 07:15:03

* State 2023-06-23 07:27:33

* State 2023-06-23 07:32:12

* State 2023-06-23 07:37:37

* State 2023-06-23 07:46:26

* State 2023-06-23 07:54:25

* State 2023-06-23 08:16:31

* State 2023-06-23 08:19:32

* State 2023-06-27 16:48:28

* State 2023-06-27 16:56:50

* State 2023-06-28 07:09:49

* State 2023-06-28 07:13:50

* State 2023-06-28 07:15:41

* State 2023-06-28 07:31:25

* State 2023-06-28 07:41:31

* State 2023-06-28 08:13:59

ja2lib/Strategic/GameEvents.c
@@ -163,16 +163,12 @@ BOOLEAN DeleteEventsWithDeletionPending() {
   return fEventDeleted;
 }
 
-void AdjustClockToEventStamp(STRATEGICEVENT *pEvent, UINT32 *puiAdjustment) {
-  UINT32 uiDiff;
-
-  uiDiff = pEvent->uiTimeStamp - GetGameTimeInSec();
+static void AdjustClockToEventStamp(STRATEGICEVENT *pEvent, UINT32 *puiAdjustment) {
+  UINT32 uiDiff = pEvent->uiTimeStamp - GetGameTimeInSec();
   MoveGameTimeForward(uiDiff);
   *puiAdjustment -= uiDiff;
 
-  swprintf(gswzWorldTimeStr, ARR_SIZE(gswzWorldTimeStr), L"%s %d, %02d:%02d",
-           gpGameClockString[STR_GAMECLOCK_DAY_NAME], GetGameTimeInDays(), GetGameClockHour(),
-           GetGameClockMinutes());
+  UpdateGameClockString2();
 }
 
 // If there are any events pending, they are processed, until the time limit is reached, or

gtrafimenkov/ja2-experiments

ja2lib/Strategic/GameEvents.c
@@ -167,16 +167,12 @@ BOOLEAN DeleteEventsWithDeletionPending() {
   return fEventDeleted;
 }
 
-void AdjustClockToEventStamp(STRATEGICEVENT *pEvent, uint32_t *puiAdjustment) {
-  uint32_t uiDiff;
-
-  uiDiff = pEvent->uiTimeStamp - GetGameTimeInSec();
+static void AdjustClockToEventStamp(STRATEGICEVENT *pEvent, uint32_t *puiAdjustment) {
+  uint32_t uiDiff = pEvent->uiTimeStamp - GetGameTimeInSec();
   MoveGameTimeForward(uiDiff);
   *puiAdjustment -= uiDiff;
 
-  swprintf(gswzWorldTimeStr, ARR_SIZE(gswzWorldTimeStr), L"%s %d, %02d:%02d",
-           gpGameClockString[STR_GAMECLOCK_DAY_NAME], GetGameTimeInDays(), GetGameClockHour(),
-           GetGameClockMinutes());
+  UpdateGameClockString2();
 }
 
 // If there are any events pending, they are processed, until the time limit is reached, or