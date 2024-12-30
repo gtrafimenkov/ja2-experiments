gtrafimenkov/ja2-rust

Move images to rust (part 1) (#32)
* State 2023-07-02 20:13:49

* State 2023-07-02 20:21:25

* State 2023-07-02 20:29:59

* State 2023-07-02 20:37:19

* State 2023-07-02 20:42:55

* State 2023-07-02 20:49:37

* State 2023-07-02 20:53:36

* State 2023-07-02 20:57:10

* State 2023-07-02 20:58:54

* State 2023-07-02 21:03:03

* State 2023-07-02 21:05:45

* State 2023-07-02 21:07:53

* State 2023-07-02 21:15:46

* State 2023-07-02 21:18:25

* State 2023-07-02 21:19:54

* State 2023-07-02 21:23:20

* State 2023-07-02 21:25:34

* State 2023-07-02 21:29:45

* State 2023-07-02 21:43:42

* State 2023-07-02 21:54:23

* State 2023-07-02 22:09:31

* State 2023-07-02 22:13:57

* State 2023-07-02 22:20:18

* State 2023-07-02 22:23:54

* State 2023-07-02 22:30:49

* State 2023-07-02 22:50:35

ja2lib/SGP/VObject.c
@@ -364,7 +364,7 @@ struct VObject *CreateVObjectFromFile(const char *path) {
   }
 
   // Check if returned himage is TRLE compressed - return error if not
-  if (!(hImage->fFlags & IMAGE_TRLECOMPRESSED)) {
+  if (!hImage->subimages) {
     MemFree(hVObject);
     DebugMsg(TOPIC_VIDEOOBJECT, DBG_NORMAL, "Invalid Image format given.");
     DestroyImage(hImage);
@@ -375,22 +375,22 @@ struct VObject *CreateVObjectFromFile(const char *path) {
   hVObject->ubBitDepth = hImage->ubBitDepth;
 
   // Get TRLE data
-  ETRLEData TempETRLEData;
-  if (!(GetETRLEImageData(hImage, &TempETRLEData))) {
+  struct ImageData TempETRLEData;
+  if (!(CopyImageData(hImage, &TempETRLEData))) {
     return FALSE;
   }
 
   // Set values
-  hVObject->usNumberOfObjects = TempETRLEData.usNumberOfObjects;
+  hVObject->number_of_subimages = TempETRLEData.number_of_subimages;
   hVObject->subimages = TempETRLEData.subimages;
-  hVObject->pPixData = TempETRLEData.pPixData;
-  hVObject->uiSizePixData = TempETRLEData.uiSizePixData;
+  hVObject->image_data = TempETRLEData.image_data;
+  hVObject->image_data_size = TempETRLEData.image_data_size;
 
   // Set palette from himage
   if (hImage->ubBitDepth == 8) {
     hVObject->pShade8 = ubColorTables[DEFAULT_SHADE_LEVEL];
     hVObject->pGlow8 = ubColorTables[0];
-    SetVideoObjectPalette(hVObject, hImage->pPalette);
+    SetVideoObjectPalette(hVObject, hImage->palette);
   }
 
   // Delete himage object
@@ -401,7 +401,7 @@ struct VObject *CreateVObjectFromFile(const char *path) {
 
 struct VObject *CreateVObjectFromHImage(struct Image *hImage) {
   struct VObject *hVObject;
-  ETRLEData TempETRLEData;
+  struct ImageData TempETRLEData;
 
   // Allocate memory for video object data and initialize
   hVObject = (struct VObject *)MemAlloc(sizeof(struct VObject));
@@ -417,7 +417,7 @@ struct VObject *CreateVObjectFromHImage(struct Image *hImage) {
   }
 
   // Check if returned himage is TRLE compressed - return error if not
-  if (!(hImage->fFlags & IMAGE_TRLECOMPRESSED)) {
+  if (!hImage->subimages) {
     MemFree(hVObject);
     DebugMsg(TOPIC_VIDEOOBJECT, DBG_NORMAL, "Invalid Image format given.");
     DestroyImage(hImage);
@@ -428,21 +428,21 @@ struct VObject *CreateVObjectFromHImage(struct Image *hImage) {
   hVObject->ubBitDepth = hImage->ubBitDepth;
 
   // Get TRLE data
-  if (!(GetETRLEImageData(hImage, &TempETRLEData))) {
+  if (!(CopyImageData(hImage, &TempETRLEData))) {
     return FALSE;
   }
 
   // Set values
-  hVObject->usNumberOfObjects = TempETRLEData.usNumberOfObjects;
+  hVObject->number_of_subimages = TempETRLEData.number_of_subimages;
   hVObject->subimages = TempETRLEData.subimages;
-  hVObject->pPixData = TempETRLEData.pPixData;
-  hVObject->uiSizePixData = TempETRLEData.uiSizePixData;
+  hVObject->image_data = TempETRLEData.image_data;
+  hVObject->image_data_size = TempETRLEData.image_data_size;
 
   // Set palette from himage
   if (hImage->ubBitDepth == 8) {
     hVObject->pShade8 = ubColorTables[DEFAULT_SHADE_LEVEL];
     hVObject->pGlow8 = ubColorTables[0];
-    SetVideoObjectPalette(hVObject, hImage->pPalette);
+    SetVideoObjectPalette(hVObject, hImage->palette);
   }
 
   return (hVObject);
@@ -503,9 +503,9 @@ BOOLEAN DeleteVideoObject(struct VObject *hVObject) {
     //		hVObject->pPaletteEntry = NULL;
   }
 
-  if (hVObject->pPixData != NULL) {
-    MemFree(hVObject->pPixData);
-    //		hVObject->pPixData = NULL;
+  if (hVObject->image_data != NULL) {
+    MemFree(hVObject->image_data);
+    //		hVObject->image_data = NULL;
   }
 
   if (hVObject->subimages != NULL) {
@@ -514,7 +514,7 @@ BOOLEAN DeleteVideoObject(struct VObject *hVObject) {
   }
 
   if (hVObject->ppZStripInfo != NULL) {
-    for (usLoop = 0; usLoop < hVObject->usNumberOfObjects; usLoop++) {
+    for (usLoop = 0; usLoop < hVObject->number_of_subimages; usLoop++) {
       if (hVObject->ppZStripInfo[usLoop] != NULL) {
         MemFree(hVObject->ppZStripInfo[usLoop]->pbZChange);
         MemFree(hVObject->ppZStripInfo[usLoop]);
@@ -728,7 +728,7 @@ BOOLEAN GetETRLEPixelValue(UINT8 *pDest, struct VObject *hVObject, UINT16 usETRL
   if (!(hVObject != NULL)) {
     return FALSE;
   }
-  if (!(usETRLEIndex < hVObject->usNumberOfObjects)) {
+  if (!(usETRLEIndex < hVObject->number_of_subimages)) {
     return FALSE;
   }
 
@@ -742,7 +742,7 @@ BOOLEAN GetETRLEPixelValue(UINT8 *pDest, struct VObject *hVObject, UINT16 usETRL
   }
 
   // Assuming everything's okay, go ahead and look...
-  pCurrent = &((UINT8 *)hVObject->pPixData)[subimages->data_offset];
+  pCurrent = &((UINT8 *)hVObject->image_data)[subimages->data_offset];
 
   // Skip past all uninteresting scanlines
   while (usLoopY < usY) {
@@ -788,7 +788,7 @@ BOOLEAN GetVideoObjectETRLEProperties(struct VObject *hVObject, struct Subimage 
   if (!(usIndex >= 0)) {
     return FALSE;
   }
-  if (!(usIndex < hVObject->usNumberOfObjects)) {
+  if (!(usIndex < hVObject->number_of_subimages)) {
     return FALSE;
   }
 

ja2lib/TileEngine/Structure.c
@@ -1687,20 +1687,20 @@ BOOLEAN AddZStripInfoToVObject(struct VObject *hVObject,
     return (TRUE);
   }
   hVObject->ppZStripInfo =
-      (ZStripInfo **)MemAlloc(sizeof(ZStripInfo *) * hVObject->usNumberOfObjects);
+      (ZStripInfo **)MemAlloc(sizeof(ZStripInfo *) * hVObject->number_of_subimages);
   if (hVObject->ppZStripInfo == NULL) {
     return (FALSE);
   }
-  memset(hVObject->ppZStripInfo, 0, sizeof(ZStripInfo *) * hVObject->usNumberOfObjects);
+  memset(hVObject->ppZStripInfo, 0, sizeof(ZStripInfo *) * hVObject->number_of_subimages);
 
   if (fFromAnimation) {
     // Determine step index for STI
     if (sSTIStartIndex == -1) {
       // one-direction only for this anim structure
-      sSTIStep = hVObject->usNumberOfObjects;
+      sSTIStep = hVObject->number_of_subimages;
       sSTIStartIndex = 0;
     } else {
-      sSTIStep = (hVObject->usNumberOfObjects / pStructureFileRef->usNumberOfStructures);
+      sSTIStep = (hVObject->number_of_subimages / pStructureFileRef->usNumberOfStructures);
     }
   } else {
     sSTIStep = 1;
@@ -1710,7 +1710,7 @@ BOOLEAN AddZStripInfoToVObject(struct VObject *hVObject,
   sNext = sSTIStartIndex + sSTIStep;
   fFirstTime = TRUE;
 
-  for (uiLoop = (UINT8)sSTIStartIndex; uiLoop < hVObject->usNumberOfObjects; uiLoop++) {
+  for (uiLoop = (UINT8)sSTIStartIndex; uiLoop < hVObject->number_of_subimages; uiLoop++) {
     // Defualt to true
     fCopyIntoVo = TRUE;
 

gtrafimenkov/ja2-experiments

ja2lib/SGP/VObject.c
@@ -368,7 +368,7 @@ struct VObject *CreateVObjectFromFile(const char *path) {
   }
 
   // Check if returned himage is TRLE compressed - return error if not
-  if (!(hImage->fFlags & IMAGE_TRLECOMPRESSED)) {
+  if (!hImage->subimages) {
     MemFree(hVObject);
     DebugMsg(TOPIC_VIDEOOBJECT, DBG_NORMAL, "Invalid Image format given.");
     DestroyImage(hImage);
@@ -379,22 +379,22 @@ struct VObject *CreateVObjectFromFile(const char *path) {
   hVObject->ubBitDepth = hImage->ubBitDepth;
 
   // Get TRLE data
-  ETRLEData TempETRLEData;
-  if (!(GetETRLEImageData(hImage, &TempETRLEData))) {
+  struct ImageData TempETRLEData;
+  if (!(CopyImageData(hImage, &TempETRLEData))) {
     return FALSE;
   }
 
   // Set values
-  hVObject->usNumberOfObjects = TempETRLEData.usNumberOfObjects;
+  hVObject->number_of_subimages = TempETRLEData.number_of_subimages;
   hVObject->subimages = TempETRLEData.subimages;
-  hVObject->pPixData = TempETRLEData.pPixData;
-  hVObject->uiSizePixData = TempETRLEData.uiSizePixData;
+  hVObject->image_data = TempETRLEData.image_data;
+  hVObject->image_data_size = TempETRLEData.image_data_size;
 
   // Set palette from himage
   if (hImage->ubBitDepth == 8) {
     hVObject->pShade8 = ubColorTables[DEFAULT_SHADE_LEVEL];
     hVObject->pGlow8 = ubColorTables[0];
-    SetVideoObjectPalette(hVObject, hImage->pPalette);
+    SetVideoObjectPalette(hVObject, hImage->palette);
   }
 
   // Delete himage object
@@ -405,7 +405,7 @@ struct VObject *CreateVObjectFromFile(const char *path) {
 
 struct VObject *CreateVObjectFromHImage(struct Image *hImage) {
   struct VObject *hVObject;
-  ETRLEData TempETRLEData;
+  struct ImageData TempETRLEData;
 
   // Allocate memory for video object data and initialize
   hVObject = (struct VObject *)MemAlloc(sizeof(struct VObject));
@@ -421,7 +421,7 @@ struct VObject *CreateVObjectFromHImage(struct Image *hImage) {
   }
 
   // Check if returned himage is TRLE compressed - return error if not
-  if (!(hImage->fFlags & IMAGE_TRLECOMPRESSED)) {
+  if (!hImage->subimages) {
     MemFree(hVObject);
     DebugMsg(TOPIC_VIDEOOBJECT, DBG_NORMAL, "Invalid Image format given.");
     DestroyImage(hImage);
@@ -432,21 +432,21 @@ struct VObject *CreateVObjectFromHImage(struct Image *hImage) {
   hVObject->ubBitDepth = hImage->ubBitDepth;
 
   // Get TRLE data
-  if (!(GetETRLEImageData(hImage, &TempETRLEData))) {
+  if (!(CopyImageData(hImage, &TempETRLEData))) {
     return FALSE;
   }
 
   // Set values
-  hVObject->usNumberOfObjects = TempETRLEData.usNumberOfObjects;
+  hVObject->number_of_subimages = TempETRLEData.number_of_subimages;
   hVObject->subimages = TempETRLEData.subimages;
-  hVObject->pPixData = TempETRLEData.pPixData;
-  hVObject->uiSizePixData = TempETRLEData.uiSizePixData;
+  hVObject->image_data = TempETRLEData.image_data;
+  hVObject->image_data_size = TempETRLEData.image_data_size;
 
   // Set palette from himage
   if (hImage->ubBitDepth == 8) {
     hVObject->pShade8 = ubColorTables[DEFAULT_SHADE_LEVEL];
     hVObject->pGlow8 = ubColorTables[0];
-    SetVideoObjectPalette(hVObject, hImage->pPalette);
+    SetVideoObjectPalette(hVObject, hImage->palette);
   }
 
   return (hVObject);
@@ -507,9 +507,9 @@ BOOLEAN DeleteVideoObject(struct VObject *hVObject) {
     //		hVObject->pPaletteEntry = NULL;
   }
 
-  if (hVObject->pPixData != NULL) {
-    MemFree(hVObject->pPixData);
-    //		hVObject->pPixData = NULL;
+  if (hVObject->image_data != NULL) {
+    MemFree(hVObject->image_data);
+    //		hVObject->image_data = NULL;
   }
 
   if (hVObject->subimages != NULL) {
@@ -518,7 +518,7 @@ BOOLEAN DeleteVideoObject(struct VObject *hVObject) {
   }
 
   if (hVObject->ppZStripInfo != NULL) {
-    for (usLoop = 0; usLoop < hVObject->usNumberOfObjects; usLoop++) {
+    for (usLoop = 0; usLoop < hVObject->number_of_subimages; usLoop++) {
       if (hVObject->ppZStripInfo[usLoop] != NULL) {
         MemFree(hVObject->ppZStripInfo[usLoop]->pbZChange);
         MemFree(hVObject->ppZStripInfo[usLoop]);
@@ -731,7 +731,7 @@ BOOLEAN GetETRLEPixelValue(uint8_t *pDest, struct VObject *hVObject, uint16_t us
   if (!(hVObject != NULL)) {
     return FALSE;
   }
-  if (!(usETRLEIndex < hVObject->usNumberOfObjects)) {
+  if (!(usETRLEIndex < hVObject->number_of_subimages)) {
     return FALSE;
   }
 
@@ -745,7 +745,7 @@ BOOLEAN GetETRLEPixelValue(uint8_t *pDest, struct VObject *hVObject, uint16_t us
   }
 
   // Assuming everything's okay, go ahead and look...
-  pCurrent = &((uint8_t *)hVObject->pPixData)[subimages->data_offset];
+  pCurrent = &((uint8_t *)hVObject->image_data)[subimages->data_offset];
 
   // Skip past all uninteresting scanlines
   while (usLoopY < usY) {
@@ -791,7 +791,7 @@ BOOLEAN GetVideoObjectETRLEProperties(struct VObject *hVObject, struct Subimage 
   if (!(usIndex >= 0)) {
     return FALSE;
   }
-  if (!(usIndex < hVObject->usNumberOfObjects)) {
+  if (!(usIndex < hVObject->number_of_subimages)) {
     return FALSE;
   }
 

ja2lib/TileEngine/Structure.c
@@ -1693,20 +1693,20 @@ BOOLEAN AddZStripInfoToVObject(struct VObject *hVObject,
     return (TRUE);
   }
   hVObject->ppZStripInfo =
-      (ZStripInfo **)MemAlloc(sizeof(ZStripInfo *) * hVObject->usNumberOfObjects);
+      (ZStripInfo **)MemAlloc(sizeof(ZStripInfo *) * hVObject->number_of_subimages);
   if (hVObject->ppZStripInfo == NULL) {
     return (FALSE);
   }
-  memset(hVObject->ppZStripInfo, 0, sizeof(ZStripInfo *) * hVObject->usNumberOfObjects);
+  memset(hVObject->ppZStripInfo, 0, sizeof(ZStripInfo *) * hVObject->number_of_subimages);
 
   if (fFromAnimation) {
     // Determine step index for STI
     if (sSTIStartIndex == -1) {
       // one-direction only for this anim structure
-      sSTIStep = hVObject->usNumberOfObjects;
+      sSTIStep = hVObject->number_of_subimages;
       sSTIStartIndex = 0;
     } else {
-      sSTIStep = (hVObject->usNumberOfObjects / pStructureFileRef->usNumberOfStructures);
+      sSTIStep = (hVObject->number_of_subimages / pStructureFileRef->usNumberOfStructures);
     }
   } else {
     sSTIStep = 1;
@@ -1716,7 +1716,7 @@ BOOLEAN AddZStripInfoToVObject(struct VObject *hVObject,
   sNext = sSTIStartIndex + sSTIStep;
   fFirstTime = TRUE;
 
-  for (uiLoop = (uint8_t)sSTIStartIndex; uiLoop < hVObject->usNumberOfObjects; uiLoop++) {
+  for (uiLoop = (uint8_t)sSTIStartIndex; uiLoop < hVObject->number_of_subimages; uiLoop++) {
     // Defualt to true
     fCopyIntoVo = TRUE;
 