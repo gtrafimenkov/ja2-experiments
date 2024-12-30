gtrafimenkov/ja2-rust

Video code cleanup (#25)
* State 2023-06-30 10:36:19

* State 2023-06-30 08:30:05

* State 2023-06-30 08:34:39

* State 2023-06-30 09:50:26

* State 2023-06-30 09:58:30

* State 2023-06-30 10:16:41

* State 2023-06-30 10:24:12

* State 2023-06-30 10:33:48

* State 2023-06-30 10:39:41

* State 2023-07-01 09:22:42

ja2lib/SGP/ImpTGA.c
@@ -62,7 +62,7 @@ BOOLEAN ReadRLERGBImage(struct Image* hImage, FileID hFile, UINT8 uiImgID, UINT8
 //
 //**************************************************************************
 
-BOOLEAN LoadTGAFileToImage(struct Image* hImage, UINT16 fContents) {
+BOOLEAN LoadTGAFileToImage(const char* filePath, struct Image* hImage, UINT16 fContents) {
   FileID hFile = FILE_ID_ERR;
   UINT8 uiImgID, uiColMap, uiType;
   UINT32 uiBytesRead;
@@ -70,11 +70,11 @@ BOOLEAN LoadTGAFileToImage(struct Image* hImage, UINT16 fContents) {
 
   Assert(hImage != NULL);
 
-  if (!(File_Exists(hImage->ImageFile))) {
+  if (!(File_Exists(filePath))) {
     return FALSE;
   }
 
-  hFile = File_OpenForReading(hImage->ImageFile);
+  hFile = File_OpenForReading(filePath);
   if (!(hFile)) {
     return FALSE;
   }

ja2lib/SGP/ImpTGA.h
@@ -4,6 +4,6 @@
 #include "SGP/HImage.h"
 #include "SGP/Types.h"
 
-BOOLEAN LoadTGAFileToImage(struct Image* hImage, UINT16 fContents);
+BOOLEAN LoadTGAFileToImage(const char* filePath, struct Image* hImage, UINT16 fContents);
 
 #endif

ja2lib/SGP/PCX.c
@@ -27,13 +27,13 @@
 BOOLEAN SetPcxPalette(PcxObject *pCurrentPcxObject, struct Image *hImage);
 BOOLEAN BlitPcxToBuffer(PcxObject *pCurrentPcxObject, UINT8 *pBuffer, UINT16 usBufferWidth,
                         UINT16 usBufferHeight, UINT16 usX, UINT16 usY, BOOLEAN fTransp);
-PcxObject *LoadPcx(CHAR8 *pFilename);
+PcxObject *LoadPcx(const char *pFilename);
 
-BOOLEAN LoadPCXFileToImage(struct Image *hImage, UINT16 fContents) {
+BOOLEAN LoadPCXFileToImage(const char *filePath, struct Image *hImage, UINT16 fContents) {
   PcxObject *pPcxObject;
 
   // First Load a PCX Image
-  pPcxObject = LoadPcx(hImage->ImageFile);
+  pPcxObject = LoadPcx(filePath);
 
   if (pPcxObject == NULL) {
     return (FALSE);
@@ -71,7 +71,7 @@ BOOLEAN LoadPCXFileToImage(struct Image *hImage, UINT16 fContents) {
   return (TRUE);
 }
 
-PcxObject *LoadPcx(STR8 pFilename) {
+PcxObject *LoadPcx(const char *pFilename) {
   PcxHeader Header;
   PcxObject *pCurrentPcxObject;
   FileID hFileHandle = FILE_ID_ERR;

ja2lib/SGP/VSurface.h
@@ -43,16 +43,6 @@ struct BltOpts {
   SGPRect FillRect;    // Given SRC subrect instead of srcregion
 };
 
-//
-// Video Surface creation flags
-// Used in the VSurface_DESC structure to describe creation flags
-//
-
-#define VSURFACE_CREATE_DEFAULT \
-  0x00000020  // Creates and empty Surface of given width, height and BPP
-#define VSURFACE_CREATE_FROMFILE \
-  0x00000040  // Creates a video Surface from a file ( using struct Image* )
-
 UINT16 GetVSurfaceHeight(const struct VSurface *vs);
 UINT16 GetVSurfaceWidth(const struct VSurface *vs);
 UINT16 *GetVSurface16BPPPalette(struct VSurface *vs);
@@ -63,11 +53,8 @@ void SetVSurface16BPPPalette(struct VSurface *vs, UINT16 *palette);
 //
 
 typedef struct {
-  UINT32 fCreateFlags;    // Specifies creation flags like from file or not
-  SGPFILENAME ImageFile;  // Filename of image data to use
-  UINT16 usWidth;         // Width, ignored if given from file
-  UINT16 usHeight;        // Height, ignored if given from file
-  UINT8 ubBitDepth;       // BPP, ignored if given from file
+  UINT16 usWidth;
+  UINT16 usHeight;
 } VSURFACE_DESC;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -84,6 +71,7 @@ BOOLEAN ShutdownVideoSurfaceManager();
 
 // Creates and adds a video Surface to list
 BOOLEAN AddVideoSurface(VSURFACE_DESC *VSurfaceDesc, VSurfID *uiIndex);
+BOOLEAN AddVideoSurfaceFromFile(const char *fileName, VSurfID *puiIndex);
 
 // Returns a HVSurface for the specified index
 BOOLEAN GetVideoSurface(struct VSurface **hVSurface, VSurfID uiIndex);
@@ -164,7 +152,7 @@ BYTE *VSurfaceLockOld(struct VSurface *vs, u32 *pitch);
 
 struct VSurface *CreateVideoSurface(u16 width, u16 height, u8 bitDepth);
 struct VSurface *CreateVideoSurfaceFromFile(const char *path);
-struct VSurface *VSurfaceAdd(u16 width, u16 height, u8 bitDepth, VSurfID *puiIndex);
+struct VSurface *VSurfaceAdd(u16 width, u16 height, VSurfID *puiIndex);
 
 // Global variables for video objects
 extern UINT32 guiRIGHTPANEL;

ja2lib/SGP/Video.h
@@ -17,7 +17,7 @@ extern void ShutdownVideoManager(void);
 extern void SuspendVideoManager(void);
 extern BOOLEAN RestoreVideoManager(void);
 
-extern void GetCurrentVideoSettings(UINT16 *usWidth, UINT16 *usHeight, UINT8 *ubBitDepth);
+extern void GetCurrentVideoSettings(UINT16 *usWidth, UINT16 *usHeight);
 
 extern void InvalidateRegion(INT32 iLeft, INT32 iTop, INT32 iRight, INT32 iBottom);
 extern void InvalidateRegionEx(INT32 iLeft, INT32 iTop, INT32 iRight, INT32 iBottom,

gtrafimenkov/ja2-experiments

ja2lib/SGP/ImpTGA.c
@@ -66,7 +66,7 @@ BOOLEAN ReadRLERGBImage(struct Image* hImage, FileID hFile, uint8_t uiImgID, uin
 //
 //**************************************************************************
 
-BOOLEAN LoadTGAFileToImage(struct Image* hImage, uint16_t fContents) {
+BOOLEAN LoadTGAFileToImage(const char* filePath, struct Image* hImage, uint16_t fContents) {
   FileID hFile = FILE_ID_ERR;
   uint8_t uiImgID, uiColMap, uiType;
   uint32_t uiBytesRead;
@@ -74,11 +74,11 @@ BOOLEAN LoadTGAFileToImage(struct Image* hImage, uint16_t fContents) {
 
   Assert(hImage != NULL);
 
-  if (!(File_Exists(hImage->ImageFile))) {
+  if (!(File_Exists(filePath))) {
     return FALSE;
   }
 
-  hFile = File_OpenForReading(hImage->ImageFile);
+  hFile = File_OpenForReading(filePath);
   if (!(hFile)) {
     return FALSE;
   }

ja2lib/SGP/ImpTGA.h
@@ -8,6 +8,6 @@
 #include "SGP/HImage.h"
 #include "SGP/Types.h"
 
-BOOLEAN LoadTGAFileToImage(struct Image* hImage, uint16_t fContents);
+BOOLEAN LoadTGAFileToImage(const char* filePath, struct Image* hImage, uint16_t fContents);
 
 #endif

ja2lib/SGP/PCX.c
@@ -31,13 +31,13 @@
 BOOLEAN SetPcxPalette(PcxObject *pCurrentPcxObject, struct Image *hImage);
 BOOLEAN BlitPcxToBuffer(PcxObject *pCurrentPcxObject, uint8_t *pBuffer, uint16_t usBufferWidth,
                         uint16_t usBufferHeight, uint16_t usX, uint16_t usY, BOOLEAN fTransp);
-PcxObject *LoadPcx(char **pFilename);
+PcxObject *LoadPcx(const char *pFilename);
 
-BOOLEAN LoadPCXFileToImage(struct Image *hImage, uint16_t fContents) {
+BOOLEAN LoadPCXFileToImage(const char *filePath, struct Image *hImage, uint16_t fContents) {
   PcxObject *pPcxObject;
 
   // First Load a PCX Image
-  pPcxObject = LoadPcx(hImage->ImageFile);
+  pPcxObject = LoadPcx(filePath);
 
   if (pPcxObject == NULL) {
     return (FALSE);
@@ -75,7 +75,7 @@ BOOLEAN LoadPCXFileToImage(struct Image *hImage, uint16_t fContents) {
   return (TRUE);
 }
 
-PcxObject *LoadPcx(char *pFilename) {
+PcxObject *LoadPcx(const char *pFilename) {
   PcxHeader Header;
   PcxObject *pCurrentPcxObject;
   FileID hFileHandle = FILE_ID_ERR;

ja2lib/SGP/VSurface.h
@@ -47,16 +47,6 @@ struct BltOpts {
   SGPRect FillRect;    // Given SRC subrect instead of srcregion
 };
 
-//
-// Video Surface creation flags
-// Used in the VSurface_DESC structure to describe creation flags
-//
-
-#define VSURFACE_CREATE_DEFAULT \
-  0x00000020  // Creates and empty Surface of given width, height and BPP
-#define VSURFACE_CREATE_FROMFILE \
-  0x00000040  // Creates a video Surface from a file ( using struct Image* )
-
 uint16_t GetVSurfaceHeight(const struct VSurface *vs);
 uint16_t GetVSurfaceWidth(const struct VSurface *vs);
 uint16_t *GetVSurface16BPPPalette(struct VSurface *vs);
@@ -67,11 +57,8 @@ void SetVSurface16BPPPalette(struct VSurface *vs, uint16_t *palette);
 //
 
 typedef struct {
-  uint32_t fCreateFlags;  // Specifies creation flags like from file or not
-  SGPFILENAME ImageFile;  // Filename of image data to use
-  uint16_t usWidth;       // Width, ignored if given from file
-  uint16_t usHeight;      // Height, ignored if given from file
-  uint8_t ubBitDepth;     // BPP, ignored if given from file
+  uint16_t usWidth;
+  uint16_t usHeight;
 } VSURFACE_DESC;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -88,6 +75,7 @@ BOOLEAN ShutdownVideoSurfaceManager();
 
 // Creates and adds a video Surface to list
 BOOLEAN AddVideoSurface(VSURFACE_DESC *VSurfaceDesc, VSurfID *uiIndex);
+BOOLEAN AddVideoSurfaceFromFile(const char *fileName, VSurfID *puiIndex);
 
 // Returns a HVSurface for the specified index
 BOOLEAN GetVideoSurface(struct VSurface **hVSurface, VSurfID uiIndex);
@@ -171,7 +159,7 @@ BYTE *VSurfaceLockOld(struct VSurface *vs, uint32_t *pitch);
 
 struct VSurface *CreateVideoSurface(uint16_t width, uint16_t height, uint8_t bitDepth);
 struct VSurface *CreateVideoSurfaceFromFile(const char *path);
-struct VSurface *VSurfaceAdd(uint16_t width, uint16_t height, uint8_t bitDepth, VSurfID *puiIndex);
+struct VSurface *VSurfaceAdd(uint16_t width, uint16_t height, VSurfID *puiIndex);
 
 // Global variables for video objects
 extern uint32_t guiRIGHTPANEL;

ja2lib/SGP/Video.h
@@ -23,7 +23,7 @@ extern void ShutdownVideoManager(void);
 extern void SuspendVideoManager(void);
 extern BOOLEAN RestoreVideoManager(void);
 
-extern void GetCurrentVideoSettings(uint16_t *usWidth, uint16_t *usHeight, uint8_t *ubBitDepth);
+extern void GetCurrentVideoSettings(uint16_t *usWidth, uint16_t *usHeight);
 
 extern void InvalidateRegion(int32_t iLeft, int32_t iTop, int32_t iRight, int32_t iBottom);
 extern void InvalidateRegionEx(int32_t iLeft, int32_t iTop, int32_t iRight, int32_t iBottom,