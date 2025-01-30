52d3d058f5db6005d057557d3f25799ff1694024
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2025-01-30 13:37:03 +0000

    (localization-support) Test size of NPCQuoteInfo

---
+++ unittester/ondisk-structures.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "TacticalAI/NPC.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+// Originally NPCQuoteInfo is 32 bytes with 4 unused bytes at the beginning in
+// Russian version and 4 unused bytes at the end in all other versions.
+TEST(OnDiskStructures, NPCQuoteInfo) { EXPECT_EQ(sizeof(NPCQuoteInfo), 32); }
