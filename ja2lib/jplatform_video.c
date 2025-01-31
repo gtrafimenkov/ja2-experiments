c0fa25a2e3ab2c73b8ba2b4cc24d411db46d5e29
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2025-01-31 20:27:44 +0000

    (video-platform) Add jplatform_video.h from ja2-experiments-vcp for reference

---
+++ ja2lib/jplatform_video.c
@@ -0,0 +1,6 @@
+#include "jplatform_video.h"
+
+void JSurface_BlitRectToRect(struct VSurface *src, struct VSurface *dst, struct JRect const *srcBox,
+                             struct JRect const *destBox) {
+  // TODO
+}
